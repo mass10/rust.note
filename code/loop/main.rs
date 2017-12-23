4bda04b617de4a9ef6a386276bc93993a733fc54
mass10 <mass10.github@gmail.com>
2017-12-23 14:28:20 +0900

    wip

--- code/loop/main.rs
+++ code/loop/main.rs
@@ -1,4 +1,31 @@
+fn test_for() {
+	
+	for i in 0..99 {
+		println!("{0}", i);
+	}
+}
+
+fn test_while() {
+
+	let mut i = 0;
+	while i < 100 {
+		println!("{}", i);
+		i += 1;
+	}
+
+	i = 0;
+	loop {
+		if 100 <= i {
+			break;
+		}
+		println!("{}", i);
+		i += 1;
+	}
+}
+
 fn main() {
 
+	test_for();
+	test_while();
 }
 
