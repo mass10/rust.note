4504aefaa3b7204d4f4c8255ad99333f7a7a1700
mass10 <mass10.github@gmail.com>
2017-12-31 14:28:55 +0900

    wip

---
+++ code/io/search/main.rs
@@ -0,0 +1,37 @@
+// use std::io;
+use std::fs::{self, DirEntry};
+use std::path::Path;
+use std::env;
+
+fn on_entry(e: &DirEntry) {
+    let path_str = String::from(e.path().as_os_str().to_str().unwrap().to_string());
+    println!("{}", path_str);
+}
+
+# [allow(unused)]
+fn visit_dirs(dir: &Path, cb: &Fn(&DirEntry)) {
+    if dir.is_dir() {
+        let it = fs::read_dir(dir).unwrap();
+        for e in it {
+            let entry = e.unwrap();
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb);
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+}
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("path?");
+        return;
+    }
+    let path = Path::new(&args[1]);
+    visit_dirs(path, &on_entry);
+}
+
