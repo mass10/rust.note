83292c25326c2c13502f89e2b9a668aa9384e1af
mass10 <mass10.github@gmail.com>
2017-12-31 23:23:17 +0900

    wip

--- code/yaml/example/src/main.rs
+++ code/yaml/example/src/main.rs
@@ -1,25 +1,17 @@
-
 use std::fs::File;
 use std::io::Read;
 
 extern crate yaml_rust;
-// use yaml_rust::{YamlLoader, YamlEmitter};
 use yaml_rust::{YamlLoader};
 
-#[allow(unused)]
+// #[allow(unused)]
 fn main() {
 
 	let mut file = File::open("conf/settings.yml").unwrap();
 	let mut content = String::new();
 	file.read_to_string(&mut content).unwrap();
-	println!("{}", content);
-
 	let docs = YamlLoader::load_from_str(content.as_str()).unwrap();
 	let doc = &docs[0];
-	println!("{:?}", doc);
 	println!("{:?}", doc["key2"].as_str().unwrap());
 	println!("{:?}", doc["unknown-key"].as_str());
-
-	println!("Ok.");
 }
-
