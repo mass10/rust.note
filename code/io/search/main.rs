95c2f6b9449669b6b472002e3f7c236e79de000b
mass10 <mass10.github@gmail.com>
2017-12-31 14:47:59 +0900

    wip

--- code/io/search/main.rs
+++ code/io/search/main.rs
@@ -4,34 +4,35 @@ use std::path::Path;
 use std::env;
 
 fn on_entry(e: &DirEntry) {
-    let path_str = String::from(e.path().as_os_str().to_str().unwrap().to_string());
-    println!("{}", path_str);
+
+	let path_str = String::from(e.path().as_os_str().to_str().unwrap().to_string());
+	println!("{}", path_str);
 }
 
 # [allow(unused)]
-fn visit_dirs(dir: &Path, cb: &Fn(&DirEntry)) {
-    if dir.is_dir() {
-        let it = fs::read_dir(dir).unwrap();
-        for e in it {
-            let entry = e.unwrap();
-            let path = entry.path();
-            if path.is_dir() {
-                visit_dirs(&path, cb);
-            } else {
-                cb(&entry);
-            }
-        }
-    }
+fn search(dir: &Path, cb: &Fn(&DirEntry)) {
+
+	if dir.is_dir() {
+		let it = fs::read_dir(dir).unwrap();
+		for e in it {
+			let entry = e.unwrap();
+			let path = entry.path();
+			if path.is_dir() {
+				search(&path, cb);
+			} else {
+				cb(&entry);
+			}
+		}
+	}
 }
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("path?");
-        return;
-    }
-    let path = Path::new(&args[1]);
-    visit_dirs(path, &on_entry);
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
+		println!("path?");
+		return;
+	}
+	let path = Path::new(&args[1]);
+	search(path, &on_entry);
 }
-
