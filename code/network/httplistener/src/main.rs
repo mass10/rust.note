51322ec09b291322bf0c9a6a53b6ed4446e8d795
mass10 <mass10.github@gmail.com>
2017-12-31 11:44:37 +0900

    wip

--- code/network/httplistener/src/main.rs
+++ code/network/httplistener/src/main.rs
@@ -10,31 +10,27 @@ use hyper::server::{Http, Request, Response, Service};
 
 struct HelloWorld;
 
-const PHRASE: &'static str = "Hello, World!";
+const PHRASE: &'static str = "{\"status\": \"Hello, World!\"}\n";
 
 impl Service for HelloWorld {
-    // boilerplate hooking up hyper's server types
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
-    // The future representing the eventual Response your call will
-    // resolve to. This can change to whatever Future you need.
-    type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
-    fn call(&self, _req: Request) -> Self::Future {
-        // We're currently ignoring the Request
-        // And returning an 'ok' Future, which means it's ready
-        // immediately, and build a Response with the 'PHRASE' body.
-        Box::new(futures::future::ok(
-            Response::new()
-                .with_header(ContentLength(PHRASE.len() as u64))
-                .with_body(PHRASE)
-        ))
-    }
+	type Request = Request;
+	type Response = Response;
+	type Error = hyper::Error;
+	type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
+
+	fn call(&self, _req: Request) -> Self::Future {
+		Box::new(futures::future::ok(
+			Response::new()
+				.with_header(ContentLength(PHRASE.len() as u64))
+				.with_body(PHRASE)
+		))
+	}
 }
 
 fn main() {
-    let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
-    server.run().unwrap();
+
+	let addr = "127.0.0.1:3000".parse().unwrap();
+	let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
+	server.run().unwrap();
 }
