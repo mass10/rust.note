440fad1e097bec96601cf4b240c9c33c9f3e3dd9
mass10 <mass10.github@gmail.com>
2018-01-01 15:20:29 +0900

    wip

---
+++ code/formatting/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    println!("Hello, world!");
+    println!("{}", "あいうえお");
+    println!("{}", 95);
+    println!("{:X}", 95);
+    println!("{:#X}", 95);
+    println!("{:x}", 95);
+    println!("{:#x}", 95);
+    println!("{:08x}", 65536);
+    println!("{:#010x}", 65536);
+    println!("{:b}", 95);
+    println!("{:#b}", 95);
+}
