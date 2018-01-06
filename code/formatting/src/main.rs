f449dfd901cc0328d20746076442d8b203e7a3e5
mass10 <mass10.github@gmail.com>
2018-01-06 22:23:31 +0900

    iwp

--- code/formatting/src/main.rs
+++ code/formatting/src/main.rs
@@ -1,14 +1,17 @@
 fn main() {
 
-    println!("Hello, world!");
-    println!("{}", "あいうえお");
-    println!("{}", 95);
-    println!("{:X}", 95);
-    println!("{:#X}", 95);
-    println!("{:x}", 95);
-    println!("{:#x}", 95);
-    println!("{:08x}", 65536);
-    println!("{:#010x}", 65536);
-    println!("{:b}", 95);
-    println!("{:#b}", 95);
+	println!("Hello, world!");
+	println!("{}", "あいうえお");
+	println!("{}", 95);
+	println!("{:X}", 95);
+	println!("{:#X}", 95);
+	println!("{:x}", 95);
+	println!("{:#x}", 95);
+	println!("{:08x}", 65536);
+	println!("{:#010x}", 65536);
+	println!("{:b}", 95);
+	println!("{:#b}", 95);
+
+	println!("family name: [{family_name}], first name: [{first_name}]", first_name="John", family_name="Jones");
+	println!("都道府県: [{都道府県}], field02: [{field02}]", field02="", 都道府県="");
 }
