7ca3ed1b7d89ee91228ea34f5b01a31896a803c4
mass10 <mass10.github@gmail.com>
2018-02-10 18:09:53 +0900

    wip

---
+++ code/string/safe-reference-after-move/src/main.rs
@@ -0,0 +1,17 @@
+fn test1(s: &String) {
+
+	println!("<test1()> {}", s);
+}
+
+fn test2(s: &String) {
+
+	println!("<test2()> {}", s);
+}
+
+fn main() {
+
+	let s = String::from("こんにちは");
+
+    test1(&s);
+    test2(&s);
+}
