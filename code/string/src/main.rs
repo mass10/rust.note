695ffb09209a97cfab2c97144a4a437c33b0a5c3
mass10 <mass10.github@gmail.com>
2018-02-10 18:04:35 +0900

    wip

--- code/string/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn diagnose(s: String) {
-	println!("{}", s);
-}
-
-fn main() {
-	let s = String::from("テスト");
-    diagnose(s); // value moved heer
-    diagnose(s); // value used here after move (COMPILATION ERROR)
-}
