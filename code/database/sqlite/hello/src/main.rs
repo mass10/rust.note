f58a79375c2c2d89f0be99a3347335dca8f48ba0
mass10 <mass10.github@gmail.com>
2018-02-12 14:11:01 +0900

    wip

--- code/database/sqlite/hello/src/main.rs
+++ code/database/sqlite/hello/src/main.rs
@@ -1,11 +1,29 @@
 extern crate sqlite;
 
-fn init() {
-
-}
-
 fn main() {
 
+	// メモリ上の仮想データベースを開きます。
 	let connection = sqlite::open(":memory:").unwrap();
-    println!("Ok.");
+
+	// 表を作成しています。
+	{
+		connection.execute("CREATE TABLE USERS(ID VARCHAR(999) NOT NULL, NAME VARCHAR(255) NOT NULL)").unwrap();
+	}
+
+	// 表にレコードを作成しています。
+	{
+		let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)").unwrap();
+		statement.bind(1, "1").unwrap();
+		statement.bind(2, "aaa").unwrap();
+		statement.next().unwrap();
+	}
+
+	// 表のレコードを抽出しています。
+	{
+		let mut statement = connection.prepare("select * from users").unwrap();
+		while let sqlite::State::Row = statement.next().unwrap() {
+			println!("id={}, name={}",
+				statement.read::<String>(0).unwrap(), statement.read::<String>(1).unwrap());
+		}
+	}
 }
