e8e7c37528809442347a1d3ca3d7f9c5e286cec6
mass10 <mass10.github@gmail.com>
2018-02-12 14:26:05 +0900

    wip

--- code/database/sqlite/hello/src/main.rs
+++ code/database/sqlite/hello/src/main.rs
@@ -14,13 +14,28 @@ fn main() {
 	{
 		let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)").unwrap();
 		statement.bind(1, "1").unwrap();
-		statement.bind(2, "aaa").unwrap();
+		statement.bind(2, "John Lennon").unwrap();
+		statement.next().unwrap();
+
+		let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)").unwrap();
+		statement.bind(1, "2").unwrap();
+		statement.bind(2, "Paul McCartney").unwrap();
+		statement.next().unwrap();
+
+		let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)").unwrap();
+		statement.bind(1, "3").unwrap();
+		statement.bind(2, "Ringo Starr").unwrap();
+		statement.next().unwrap();
+
+		let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)").unwrap();
+		statement.bind(1, "4").unwrap();
+		statement.bind(2, "George Harrison").unwrap();
 		statement.next().unwrap();
 	}
 
 	// 表のレコードを抽出しています。
 	{
-		let mut statement = connection.prepare("select * from users").unwrap();
+		let mut statement = connection.prepare("SELECT * FROM USERS").unwrap();
 		while let sqlite::State::Row = statement.next().unwrap() {
 			println!("id={}, name={}",
 				statement.read::<String>(0).unwrap(), statement.read::<String>(1).unwrap());
