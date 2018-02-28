3e107d605b8f3fb701265cc656f89e52cd7713e3
mass10 <mass10.github@gmail.com>
2018-02-28 22:46:39 +0900

    wip

--- code/filesystem/reading-text-file/src/main.rs
+++ code/filesystem/reading-text-file/src/main.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::io::BufRead;
 
 fn read_text_file(path: &str) -> String {
 
@@ -10,6 +11,19 @@ fn read_text_file(path: &str) -> String {
 
 fn main() {
 
-	let s = read_text_file("src/main.rs");
-	println!("{}", s);
+	// reading whole text file.
+	{
+		let s = read_text_file("src/main.rs");
+		println!("{}", s);
+	}
+
+	// reading a text file line-by-line.
+	{
+		let path = "src/main.rs";
+		let file = std::fs::File::open(path).unwrap();
+		let r = std::io::BufReader::new(file);
+		for line in r.lines() {
+			println!("{}", line.unwrap());
+		}
+	}
 }
