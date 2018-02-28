03e35075914ab7834ad434fb64d05681b6ca9f5f
mass10 <mass10.github@gmail.com>
2018-02-28 21:59:01 +0900

    wip

---
+++ code/filesystem/enumerate/main.rs
@@ -0,0 +1,38 @@
+// use std::io;
+use std::fs::{self, DirEntry};
+use std::path::Path;
+use std::env;
+
+fn on_entry(e: &DirEntry) {
+
+	let path_str = String::from(e.path().as_os_str().to_str().unwrap().to_string());
+	println!("{}", path_str);
+}
+
+# [allow(unused)]
+fn search(dir: &Path, handler: &Fn(&DirEntry)) {
+
+	if dir.is_dir() {
+		let it = fs::read_dir(dir).unwrap();
+		for e in it {
+			let entry = e.unwrap();
+			let path = entry.path();
+			if path.is_dir() {
+				search(&path, handler);
+			} else {
+				handler(&entry);
+			}
+		}
+	}
+}
+
+fn main() {
+
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
+		println!("path?");
+		return;
+	}
+	let path = Path::new(&args[1]);
+	search(path, &on_entry);
+}
