c04ee84a13598494328092cc23b72a75d98c089b
mass10 <mass10.github@gmail.com>
2018-02-28 21:20:30 +0900

    wip

--- code/functions/main.rs
+++ code/functions/main.rs
@@ -2,7 +2,14 @@ fn test1(s: &str) {
 	println!("{}", s)
 }
 
+fn create_string() -> String {
+
+	return String::from("Hello, Real Rust World!");
+}
+
 fn main() {
-	test1("abc")
+
+	test1("abc");
+	println!("{:?}", create_string());
 }
 
