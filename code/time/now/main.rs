76e076e235b610af307f6e4b17ce414eddd1a9eb
mass10 <mass10.github@gmail.com>
2018-03-04 16:51:28 +0900

    wip

--- code/time/now/main.rs
+++ code/time/now/main.rs
@@ -2,7 +2,14 @@ use std::time;
 
 fn main() {
 
-	let now = time::Instant::now();
-	println!("{:?}", now);
-}
+	{
+		let now = time::Instant::now();
+		println!("{:?}", now);
+	}
 
+	{
+		let now = time::SystemTime::now();
+		println!("{:?}", now);
+		// println!("{:?}", time::strftime(now).unwrap());
+	}
+}
