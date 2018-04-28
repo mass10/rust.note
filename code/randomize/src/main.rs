4c01040d3005c02c03aaf6809e1ba1fa58a3f44b
mass10 <mass10.github@gmail.com>
2018-04-28 14:29:38 +0900

    wip

--- code/randomize/src/main.rs
+++ code/randomize/src/main.rs
@@ -1,11 +1,12 @@
 // use std::fs;
 use std::path::Path;
+use std::ffi::OsStr;
 // use std::env;
 
 extern crate uuid;
 use uuid::Uuid;
 
-fn _gen() -> String {
+fn _generate_new_name() -> String {
 
 	let uuid = Uuid::new_v4();
 	return uuid.hyphenated().to_string();
@@ -13,19 +14,24 @@ fn _gen() -> String {
 
 fn on_file_found(e: &Path) -> std::io::Result<()> {
 
-	let path_str = e.as_os_str().to_str().unwrap();
+	let _path_str = e.as_os_str().to_str().unwrap();
 	let parent = match e.parent() {
 		Some(result) => result,
 		None => Path::new("")
 	};
-	let name = ""; //e.file_stem().unwrap().to_str().unwrap();
-	let extension = ""; //e.extension().unwrap().to_str().unwrap();
-	println!("{}, {}, {}", parent.as_os_str().to_str().unwrap(), name, extension);
+	let name = _generate_new_name();
+	let ext = match e.extension() {
+		Some(result1) => result1,
+		None => OsStr::new("")
+	};
+	let new_path = parent.join(&name).with_extension(ext);
+	println!("{}", new_path.as_os_str().to_str().unwrap());
+	std::fs::rename(e, new_path)?;
 	Ok(())
 }
 
-# [allow(unused)]
-fn search(e: &Path, handler: &Fn(&Path) -> std::io::Result<()>) -> std::io::Result<()> {
+#[allow(unused)]
+fn enumerate(e: &Path, handler: &Fn(&Path) -> std::io::Result<()>) -> std::io::Result<()> {
 
 	if !e.exists() {
 		println!("[TRACE] invalid path {}", e.to_str().unwrap());
@@ -36,7 +42,7 @@ fn search(e: &Path, handler: &Fn(&Path) -> std::io::Result<()>) -> std::io::Resu
 		for e in it {
 			let entry = e.unwrap();
 			let path = entry.path();
-			search(&path, handler);
+			enumerate(&path, handler);
 		}
 	}
 	else {
@@ -52,6 +58,8 @@ fn main() {
 		println!("path?");
 		return;
 	}
-	let path = Path::new(&args[1]);
-	let _result = search(path, &on_file_found);
+	for e in &args[1..] {
+		let path = Path::new(e);
+		let _ = enumerate(path, &on_file_found);
+	}
 }
