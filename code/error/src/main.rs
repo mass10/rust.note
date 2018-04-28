713c7a9bbc1d66fc88b9899b6211dc0aea1868b8
mass10 <mass10.github@gmail.com>
2018-04-28 15:35:18 +0900

    wip

--- code/error/src/main.rs
+++ code/error/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
 
 	let left = std::path::Path::new("left");
 	let right = std::path::Path::new("right");
+
+	// rename() はエラー情報を返すが、拾わなくても panic しない
 	let result = std::fs::rename(left, right);
 	match result {
 		Ok(n) => println!("success: {:?}", n),
