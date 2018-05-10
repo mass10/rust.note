92a3d8b4777ded2fa1ae57eb8b0fa6795c0d87f3
mass10 <mass10.github@gmail.com>
2018-05-11 00:17:49 +0900

    wip

--- code/yaml/example/src/main.rs
+++ code/yaml/example/src/main.rs
@@ -4,13 +4,18 @@ use std::fs::File;
 use std::io::Read;
 use yaml_rust::yaml::YamlLoader;
 
-// #[allow(unused)]
-fn main() {
+fn configure() -> Vec<yaml_rust::Yaml> {
 
 	let mut file = File::open("conf/settings.yml").unwrap();
 	let mut content = String::new();
 	file.read_to_string(&mut content).unwrap();
 	let docs = YamlLoader::load_from_str(content.as_str()).unwrap();
+	return docs;
+}
+
+fn main() {
+
+	let docs = configure();
 	let doc = &docs[0];
 
 	// println!("{:?}", doc["key1"].as_str().unwrap());
