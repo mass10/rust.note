b341792e74090ae927a2029b92c30a2b04ecf36c
mass10 <mass10.github@gmail.com>
2018-05-13 16:08:53 +0900

    wip

--- code/vector/main.rs
+++ code/vector/main.rs
@@ -1,3 +1,18 @@
+struct Class {
+}
+
+impl Class {
+	fn enum_students(&self) -> Vec<String> {
+		let mut v: Vec<String> = vec![];
+		v.push(String::from("jimi.hendrix@gmail.com"));
+		v.push(String::from("paul.kossoff@gmail.com"));
+		v.push(String::from("freddie.mercury@gmail.com"));
+		v.push(String::from("billy.preston@gmail.com"));
+		v.push(String::from("steve.marriot@gmail.com"));
+		return v;
+	}
+}
+
 fn test(v: &Vec<i64>) {
 	print!("[TRACE] ");
 	for (i, e) in v.iter().enumerate() {
@@ -28,4 +43,8 @@ fn main() {
 
 	let v = vec![1, 3, -89, 67, 10092, -1029, -12, -10];
 	println!("3つめの要素は {0} です。", v[2]);
+
+	let class = Class{};
+	println!("{:?}", class.enum_students());
+	println!("{:?}", class.enum_students());
 }
