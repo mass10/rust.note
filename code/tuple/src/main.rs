2fd788dbfa2e5f98d284fb39a13867319b19cb52
mass10 <mass10.github@gmail.com>
2018-05-14 22:11:39 +0900

    wip

--- code/tuple/src/main.rs
+++ code/tuple/src/main.rs
@@ -1,9 +1,28 @@
 fn get_current_position() -> (i64, i64) {
-	let items = (1, 1272);
-	return items;
+
+	return (1, 1272);
 }
 
 fn main() {
-	let position = get_current_position();
-	println!("{:?}", position);
+
+	{
+		let position = get_current_position();
+		// shows (left, right)
+		println!("{:?}", position);
+		// shows left one.
+		println!("{}", position.0);
+		// shows right one.
+		println!("{}", position.1);
+		// immutable
+		// position.0 = 0;
+		// compilation fails.
+		// println!("{:?}", position.2);
+	}
+
+	{
+		let mut position = get_current_position();
+		// mutable
+		position.1 = position.1 + 100;
+		println!("{:?}", position);
+	}
 }
