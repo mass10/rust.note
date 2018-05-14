e1cdbb0e89d80fd4042704cdeb3721f993ca9890
mass10 <mass10.github@gmail.com>
2018-05-14 23:27:29 +0900

    wip

--- code/time/elapsed/src/main.rs
+++ code/time/elapsed/src/main.rs
@@ -1,12 +1,34 @@
 use std::time;
 use std::thread;
 
+trait formatting {
+	fn to_string(&self) -> String;
+}
+
+impl formatting for time::Duration {
+	fn to_string(&self) -> String {
+		let mut sec = self.as_secs();
+		let mut min = 0;
+		let mut hour = 0;
+		while 60 <= sec {
+			min += 1;
+			sec -= 60;
+		}
+		while 60 <= min {
+			hour += 1;
+			min -= 60;
+		}
+		let s = format!("{:02}:{:02}:{:02}", hour, min, sec);
+		return s;
+	}
+}
+
 fn duration(left: time::Instant, right: time::Instant) {
 
 	let elapsed = right - left;
 
 	println!("{:?}", elapsed);
-	println!("{}", elapsed.as_secs());
+	println!("{}", elapsed.to_string());
 	println!("{}", elapsed.subsec_nanos());
 }
 
