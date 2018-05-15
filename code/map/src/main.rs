869296e031445ea1ef70bfb30a65208ce50b2c35
mass10 <mass10.github@gmail.com>
2018-05-15 23:34:21 +0900

    wip

--- code/map/src/main.rs
+++ code/map/src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
 
-    let v = vec![111, 222, 333, 444, 555];
-    for e in v.into_iter().map(|x| x * 2) {
-    	println!("{:?}", e);
-    }
+	let v = vec![11, 22, 33, 44, 55];
+	for e in v.into_iter().map(|x| x * 2) {
+		println!("{:?}", e);
+	}
+
+	let v = vec![11, 22, 33, 44, 55];
+	for e in v.into_iter() {
+		println!("{:?}", e);
+	}
 }
