6d9943d2549d8f0eba1aa46c823bba9b120be605
mass10 <mass10.github@gmail.com>
2018-05-15 23:35:53 +0900

    wip

--- code/map/src/main.rs
+++ code/map/src/main.rs
@@ -1,12 +1,14 @@
 fn main() {
 
+	// v を繰り返し e で受け取る
 	let v = vec![11, 22, 33, 44, 55];
-	for e in v.into_iter().map(|x| x * 2) {
+	for e in v.into_iter() {
 		println!("{:?}", e);
 	}
 
+	// v を x * 2 したものを繰り返し e で受け取る
 	let v = vec![11, 22, 33, 44, 55];
-	for e in v.into_iter() {
+	for e in v.into_iter().map(|x| x * 2) {
 		println!("{:?}", e);
 	}
 }
