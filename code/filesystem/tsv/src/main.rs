16756a95962d946f52037d51df98cc039d9855b2
mass10 <mass10.github@gmail.com>
2018-05-18 01:01:48 +0900

    wip

---
+++ code/filesystem/tsv/src/main.rs
@@ -0,0 +1,27 @@
+extern crate csv;
+
+fn read_main<R: std::io::Read>(stream: R) {
+
+	let mut reader = csv::ReaderBuilder::new().delimiter(b'\t').from_reader(stream);
+	for row in reader.records() {
+		if row.is_err() {
+			continue;
+		}
+		let record = row.unwrap();
+		println!("date: {:?}, mail: {:?}, name: {:?}",
+			record[0].to_string(), record[1].to_string(), record[2].to_string());
+	}
+}
+
+fn main() {
+
+	if false {
+		let path = std::path::Path::new("sample.csv");
+		let file = std::fs::File::open(path).unwrap();
+		read_main(file);
+	}
+
+	if true {
+		read_main(std::io::stdin());
+	}
+}
