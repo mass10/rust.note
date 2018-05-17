515f24575777d0724a010b88366cdde1fab8251a
mass10 <mass10.github@gmail.com>
2018-05-18 01:04:35 +0900

    wip

--- code/time/systemtime/src/main.rs
+++ code/time/systemtime/src/main.rs
@@ -1,3 +1,5 @@
+extern crate chrono;
+
 use std::time::SystemTime;
 
 fn main() {
@@ -5,4 +7,9 @@ fn main() {
 	let now = SystemTime::now();
 	println!("{:?}", now);
 	// println!("{:#X}", now);
+
+	let date = chrono::Local::now();
+	println!("{}", date.format("%Y-%m-%d %H:%M:%S%.3f"));
+    println!("{}", date.format("%+"));
 }
+
