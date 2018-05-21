4dbe1655276104dffce24af60c619357cf445711
mass10 <mass10.github@gmail.com>
2018-05-21 22:34:42 +0900

    wip

--- code/time/systemtime/src/main.rs
+++ code/time/systemtime/src/main.rs
@@ -4,12 +4,17 @@ use std::time::SystemTime;
 
 fn main() {
 
-	let now = SystemTime::now();
-	println!("{:?}", now);
-	// println!("{:#X}", now);
+	{
+		let now = SystemTime::now();
+		println!("{:?}", now);
+	}
 
-	let date = chrono::Local::now();
-	println!("{}", date.format("%Y-%m-%d %H:%M:%S%.3f"));
-    println!("{}", date.format("%+"));
+	{
+		let date = chrono::Local::now();
+		// timestamp with timezone
+		println!("{}", date.format("%+"));
+		// timestamp
+		println!("{}", date.format("%Y-%m-%d %H:%M:%S%.3f"));
+	}
 }
 
