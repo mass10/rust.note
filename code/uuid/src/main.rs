d4dd05a946e7387323eb61ce69cc3045ba9ccff9
mass10 <mass10.github@gmail.com>
2018-05-27 23:22:32 +0900

    wip

--- code/uuid/src/main.rs
+++ code/uuid/src/main.rs
@@ -5,7 +5,10 @@ use uuid::Uuid;
 fn generate_uuid4() -> String {
 
 	let uuid = Uuid::new_v4();
-	return format!("{}", uuid);
+	let l1 = uuid.hyphenated().to_string();
+	let l2 = format!("{}", uuid);
+	assert_eq!(l1, l2);
+	return l2;
 }
 
 fn main() {
@@ -18,4 +21,8 @@ fn main() {
 
 	let uuid = generate_uuid4();
 	println!("{}", uuid);
+
+	// all zero
+	let uuid = Uuid::nil();
+	println!("{}", uuid.hyphenated().to_string());
 }
