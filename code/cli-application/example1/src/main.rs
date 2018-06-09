2aa6ce6e435e25d8a9a730915b4cedeaf0ac7033
mass10 <mass10.github@gmail.com>
2018-06-09 10:35:47 +0900

    wip

--- code/cli-application/example1/src/main.rs
+++ code/cli-application/example1/src/main.rs
@@ -27,6 +27,7 @@ fn main() {
 
 	// retrieving
 	let matches = application.get_matches();
+	// println!("{:?}", matches);
 	let configuration_path = matches.value_of("path to configuration file").unwrap_or(".settings.yaml");
 	let verbose_option = matches.is_present("verbose option");
 	println!("config: {}", configuration_path);
