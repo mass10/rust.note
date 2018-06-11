d6d7ac345dadc7d2e450e4ea34c24a94ad21c7f4
mass10 <mass10.github@gmail.com>
2018-06-12 00:18:12 +0900

    wip

--- code/database/sqlite/hello/src/main.rs
+++ code/database/sqlite/hello/src/main.rs
@@ -1,44 +1,76 @@
 extern crate sqlite;
 
+struct Application {
+
+}
+
+impl Application {
+
+	pub fn new() -> Application {
+	
+		let app = Application{};
+		return app;
+	}
+
+	pub fn open() -> sqlite::Connection {
+
+		let connection = sqlite::open(":memory:").unwrap();
+		return connection;
+	}
+
+	pub fn run(&mut self) -> std::result::Result<String, sqlite::Error> {
+
+		let connection = Application::open();
+
+		// 表を作成しています。
+		{
+			connection.execute("CREATE TABLE USERS(ID VARCHAR(999) NOT NULL, NAME VARCHAR(255) NOT NULL)")?;
+		}
+
+		// 表にレコードを作成しています。
+		{
+			let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)")?;
+			statement.bind(1, "1")?;
+			statement.bind(2, "John Lennon")?;
+			statement.next()?;
+
+			let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)")?;
+			statement.bind(1, "2")?;
+			statement.bind(2, "Paul McCartney")?;
+			statement.next()?;
+
+			let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)")?;
+			statement.bind(1, "3")?;
+			statement.bind(2, "Ringo Starr")?;
+			statement.next()?;
+
+			let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)")?;
+			statement.bind(1, "4")?;
+			statement.bind(2, "George Harrison")?;
+			statement.next()?;
+		}
+
+		// 表のレコードを抽出しています。
+		{
+			let mut statement = connection.prepare("SELECT * FROM USERS")?;
+			while let sqlite::State::Row = statement.next()? {
+				println!("id={}, name={}",
+					statement.read::<String>(0)?, statement.read::<String>(1)?);
+			}
+		}
+
+		return Ok(String::from("DONE"));
+	}
+}
+
 fn main() {
 
 	// メモリ上の仮想データベースを開きます。
-	let connection = sqlite::open(":memory:").unwrap();
-
-	// 表を作成しています。
-	{
-		connection.execute("CREATE TABLE USERS(ID VARCHAR(999) NOT NULL, NAME VARCHAR(255) NOT NULL)").unwrap();
+	let mut app = Application::new();
+	let result = app.run();
+	if result.is_err() {
+		println!("[ERROR] {:?}", result.err().unwrap());
+		return;
 	}
-
-	// 表にレコードを作成しています。
-	{
-		let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)").unwrap();
-		statement.bind(1, "1").unwrap();
-		statement.bind(2, "John Lennon").unwrap();
-		statement.next().unwrap();
-
-		let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)").unwrap();
-		statement.bind(1, "2").unwrap();
-		statement.bind(2, "Paul McCartney").unwrap();
-		statement.next().unwrap();
-
-		let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)").unwrap();
-		statement.bind(1, "3").unwrap();
-		statement.bind(2, "Ringo Starr").unwrap();
-		statement.next().unwrap();
-
-		let mut statement = connection.prepare("INSERT INTO USERS(ID, NAME) VALUES(?, ?)").unwrap();
-		statement.bind(1, "4").unwrap();
-		statement.bind(2, "George Harrison").unwrap();
-		statement.next().unwrap();
-	}
-
-	// 表のレコードを抽出しています。
-	{
-		let mut statement = connection.prepare("SELECT * FROM USERS").unwrap();
-		while let sqlite::State::Row = statement.next().unwrap() {
-			println!("id={}, name={}",
-				statement.read::<String>(0).unwrap(), statement.read::<String>(1).unwrap());
-		}
-	}
+	println!("[INFO] {}", result.unwrap());
 }
