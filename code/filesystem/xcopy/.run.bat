fc921be1e4b1615fa6e7a247fb515edbcf18d451
Irisawa Masaru <mass10.github@gmail.com>
2020-04-07 22:28:44 +0900

    wip

---
+++ code/filesystem/xcopy/.run.bat
@@ -0,0 +1,3 @@
+@REM cargo run %1 %2
+@REM cargo run --release C:\tmp\techtouch-player-0 C:\tmp\techtouch-player-duplicated
+@cargo run --release "C:\Users\irisa\workspace\techtouch-frontend" "C:\Users\irisa\workspace\techtouch-frontend-for-linux"
