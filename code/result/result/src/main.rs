b38870da450b1b85606c80e03517f7a17a77f4db
Irisawa Masaru <mass10.github@gmail.com>
2020-04-11 10:57:26 +0900

    Update main.rs

--- code/result/result/src/main.rs
+++ code/result/result/src/main.rs
@@ -1,31 +1,29 @@
-fn run(s: &str) -> std::result::Result<String, String> {
-
+fn diagnose_request(s: &str) -> std::result::Result<String, String> {
 	let s = s.to_string().to_uppercase();
 	let s = s.as_str();
 	match s {
-		"OK" => Ok("LGTM!!".to_string()),
-		_ => Err("Boooo..".to_string()),
+		"ALL MY LOVING" => Ok(String::from("The Beatles")),
+		"SHE'S NOT THERE" => Ok(String::from("The Zombies")),
+		"BREAKING THE LAW" => Err(String::from("Judas Priest")),
+		"WAR PIGS" => Ok(String::from("Black Sabbath")),
+		"PAINKILLER" => Err(String::from("Judas Priest")),
+		_ => Err(String::from("You go home, boy!")),
 	}
 }
 
 fn main() {
+	// コマンドライン引数を結合し、リクエストとします。
+	let args: Vec<String> = std::env::args().skip(1).collect();
+	let request_string = args.join(" ");
 
-	let args: Vec<String> = std::env::args().collect();
-
-	// 1st argument
-	let s = match 2 <= args.len() {
-		true => &args[1],
-		_ => "",
-	};
-
-	// test
-	let result = run(s);
+	// リクエストを読み取ります。
+	let result = diagnose_request(request_string.as_str());
 	if result.is_err() {
 		let answer = result.err().unwrap();
-		println!("[TRACE] NG! {}", answer);
+		println!("[Err] {}", answer);
 		return;
 	}
 
 	let response = result.unwrap();
-	println!("[TRACE] OK. {}", response);
+	println!("[Ok] {}", response);
 }
