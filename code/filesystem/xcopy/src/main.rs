bd7da27033c984dc30247da2102d94d1f89c7273
Irisawa Masaru <mass10.github@gmail.com>
2020-04-11 22:52:30 +0900

    wip

--- code/filesystem/xcopy/src/main.rs
+++ code/filesystem/xcopy/src/main.rs
@@ -1,35 +1,37 @@
-/// 
-/// 
+use file_diff::diff;
+///
+///
 /// 日本語パス名への対応が未確認です。
-/// 
 ///
 
-use std::path::Path;
-use file_diff::{diff};
-
-/// プロンプトを表示し、YES/NO の応答を読み取ります。
-fn prompt() -> bool {
-
+/// 標準入力から一行の入力を得ます。
+fn input() -> String {
 	let mut line = String::new();
 	let ret = std::io::stdin().read_line(&mut line);
 	if ret.is_err() {
-		return false;
+		println!("[ERROR] {}", ret.err().unwrap());
+		return String::new();
 	}
 	if ret.unwrap() == 0 {
-		return false;
+		return String::new();
 	}
-	if line.trim().to_uppercase() == "Y" {
+	return (*line.trim()).to_string();
+}
+
+/// プロンプトを表示し、YES/NO の応答を読み取ります。
+fn confirm() -> bool {
+	let line = input();
+	if line.to_uppercase() == "Y" {
 		return true;
 	}
-	if line.trim().to_uppercase() == "YES" {
+	if line.to_uppercase() == "YES" {
 		return true;
 	}
 	return false;
 }
 
 /// 二つのファイルが同一かどうかを調べます。
-fn seems_to_be_same(source_path: &Path, destination_path: &Path) -> bool {
-
+fn seems_to_be_same(source_path: &std::path::Path, destination_path: &std::path::Path) -> bool {
 	// 元
 	let left = std::fs::metadata(source_path).unwrap();
 	// 先
@@ -42,21 +44,26 @@ fn seems_to_be_same(source_path: &Path, destination_path: &Path) -> bool {
 	// サイズとタイムスタンプが同じなら同じとみなします。
 	if left.len() == right.len() {
 		if left.modified().unwrap() == right.modified().unwrap() {
-			return true
+			return true;
 		}
 	}
 	// 中身を比較
-	return diff(source_path.to_str().unwrap(), destination_path.to_str().unwrap());
+	return diff(
+		source_path.to_str().unwrap(),
+		destination_path.to_str().unwrap(),
+	);
 }
 
 /// ファイルごとに呼びだされるハンドラーです。
-fn on_entry(source_path: &Path, destination_path: &Path) -> std::io::Result<()> {
-
-	if seems_to_be_same(source_path, destination_path) {
+fn copy_file(source_path: &str, destination_path: &str) -> std::io::Result<()> {
+	if seems_to_be_same(
+		std::path::Path::new(source_path),
+		std::path::Path::new(destination_path),
+	) {
 		return Ok(());
 	}
-	println!("ファイル {} を上書きしますか？(y/N)", destination_path.as_os_str().to_str().unwrap());
-	if !prompt() {
+	println!("ファイル {} を上書きしますか？(y/N)", destination_path);
+	if !confirm() {
 		return Ok(());
 	}
 	let _result = std::fs::copy(source_path, destination_path);
@@ -64,8 +71,13 @@ fn on_entry(source_path: &Path, destination_path: &Path) -> std::io::Result<()> 
 }
 
 /// ディレクトリをコピーします。
-fn xcopy(source_path: &Path, destination_path: &Path, handler: &dyn Fn(&Path, &Path) -> std::io::Result<()>) -> std::io::Result<()> {
-
+fn xcopy(
+	source_path: &str,
+	destination_path: &str,
+	handler: &dyn Fn(&str, &str) -> std::io::Result<()>,
+) -> std::io::Result<()> {
+	let source_path = std::path::Path::new(source_path);
+	let destination_path = std::path::Path::new(destination_path);
 	if !source_path.exists() {
 		println!("[TRACE] invalid path {}", source_path.to_str().unwrap());
 		return Ok(());
@@ -76,6 +88,27 @@ fn xcopy(source_path: &Path, destination_path: &Path, handler: &dyn Fn(&Path, &P
 			return Ok(());
 		}
 		if dir_name == ".git" {
+			return Ok(());
+		}
+		if dir_name == ".project" {
+			return Ok(());
+		}
+		if dir_name == "dist" {
+			return Ok(());
+		}
+		if dir_name == ".nuxt" {
+			return Ok(());
+		}
+		if dir_name == ".vscode" {
+			return Ok(());
+		}
+		if dir_name == ".circleci" {
+			return Ok(());
+		}
+		if dir_name == ".github" {
+			return Ok(());
+		}
+		if dir_name == "env" {
 			return Ok(());
 		}
 		// コピー先にディレクトリを作成します。
@@ -91,35 +124,45 @@ fn xcopy(source_path: &Path, destination_path: &Path, handler: &dyn Fn(&Path, &P
 			let entry = e.unwrap();
 			let name = entry.file_name();
 			let path = entry.path();
-			let _ = xcopy(&path, destination_path.join(name).as_path(), handler);
+			let _ = xcopy(
+				&path.to_str().unwrap(),
+				destination_path.join(name).as_path().to_str().unwrap(),
+				handler,
+			);
 		}
 		return Ok(());
 	}
 	if source_path.is_file() {
-		let file_name = source_path.file_name().unwrap();
+		let file_name = source_path.file_name().unwrap().to_str().unwrap();
 		if file_name == ".gitignore" {
-			return Ok(())
+			return Ok(());
 		}
 		if file_name == ".project" {
-			return Ok(())
+			return Ok(());
 		}
-		if file_name.to_os_string().to_str().unwrap().starts_with(".env.") {
-			return Ok(())
+		if file_name == "makefile" {
+			return Ok(());
 		}
-		return handler(source_path, destination_path);
+		if file_name == "yarn-error.log" {
+			return Ok(());
+		}
+		if file_name.starts_with(".env.") {
+			return Ok(());
+		}
+		return handler(
+			source_path.to_str().unwrap(),
+			destination_path.to_str().unwrap(),
+		);
 	}
-	return Ok(())
+	return Ok(());
 }
 
 /// エントリーポイントです。
 fn main() {
-
 	let args: Vec<String> = std::env::args().collect();
 	if args.len() < 3 {
 		println!("path?");
 		return;
 	}
-	let path_source = Path::new(&args[1]);
-	let path_destination = Path::new(&args[2]);
-	let _ = xcopy(path_source, path_destination, &on_entry);
+	let _ = xcopy(&args[1], &args[2], &copy_file);
 }
