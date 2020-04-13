78c23195249c88a10ec4513ffc04bffa56da0dc1
Irisawa Masaru <mass10.github@gmail.com>
2020-04-14 07:41:45 +0900

    wip

--- code/filesystem/xcopy/src/main.rs
+++ code/filesystem/xcopy/src/main.rs
@@ -31,42 +31,49 @@ fn confirm() -> bool {
 }
 
 /// 二つのファイルが同一かどうかを調べます。
-fn seems_to_be_same(source_path: &std::path::Path, destination_path: &std::path::Path) -> bool {
+fn seems_to_be_same(
+	source_path: &std::path::Path,
+	destination_path: &std::path::Path,
+) -> std::result::Result<bool, Box<dyn std::error::Error>> {
 	// 元
-	let left = std::fs::metadata(source_path).unwrap();
+	let left = std::fs::metadata(source_path)?;
 	// 先
 	let right_attribute = std::fs::metadata(destination_path);
 	if right_attribute.is_err() {
 		// 先のファイルがみつからないようです。
-		return false;
+		return Ok(false);
 	}
-	let right = right_attribute.unwrap();
+	let right = right_attribute?;
 	// サイズとタイムスタンプが同じなら同じとみなします。
 	if left.len() == right.len() {
-		if left.modified().unwrap() == right.modified().unwrap() {
-			return true;
+		if left.modified()? == right.modified()? {
+			return Ok(true);
 		}
 	}
 	// 中身を比較
-	return diff(
+	let result = diff(
 		source_path.to_str().unwrap(),
 		destination_path.to_str().unwrap(),
 	);
+	return Ok(result);
 }
 
 /// ファイルごとに呼びだされるハンドラーです。
-fn copy_file(source_path: &str, destination_path: &str) -> std::io::Result<()> {
+fn copy_file(
+	source_path: &str,
+	destination_path: &str,
+) -> std::result::Result<(), Box<dyn std::error::Error>> {
 	if seems_to_be_same(
 		std::path::Path::new(source_path),
 		std::path::Path::new(destination_path),
-	) {
+	)? {
 		return Ok(());
 	}
 	println!("ファイル {} を上書きしますか？(y/N)", destination_path);
 	if !confirm() {
 		return Ok(());
 	}
-	let _result = std::fs::copy(source_path, destination_path);
+	std::fs::copy(source_path, destination_path)?;
 	return Ok(());
 }
 
@@ -74,8 +81,8 @@ fn copy_file(source_path: &str, destination_path: &str) -> std::io::Result<()> {
 fn xcopy(
 	source_path: &str,
 	destination_path: &str,
-	handler: &dyn Fn(&str, &str) -> std::io::Result<()>,
-) -> std::io::Result<()> {
+	handler: &dyn Fn(&str, &str) -> std::result::Result<(), Box<dyn std::error::Error>>,
+) -> std::result::Result<(), Box<dyn std::error::Error>> {
 	let source_path = std::path::Path::new(source_path);
 	let destination_path = std::path::Path::new(destination_path);
 	if !source_path.exists() {
@@ -83,7 +90,7 @@ fn xcopy(
 		return Ok(());
 	}
 	if source_path.is_dir() {
-		let dir_name = source_path.file_name().unwrap();
+		let dir_name = source_path.file_name().unwrap().to_str().unwrap();
 		if dir_name == "node_modules" {
 			return Ok(());
 		}
@@ -94,16 +101,11 @@ fn xcopy(
 			return Ok(());
 		}
 		// コピー先にディレクトリを作成します。
-		let result = std::fs::create_dir_all(destination_path);
-		if result.is_err() {
-			let error = result.err().unwrap();
-			println!("[ERROR] reason: {}", error);
-			return Ok(());
-		}
+		std::fs::create_dir_all(destination_path)?;
 		// ディレクトリ内エントリーを走査
 		let it = std::fs::read_dir(source_path)?;
 		for e in it {
-			let entry = e.unwrap();
+			let entry = e?;
 			let name = entry.file_name();
 			let path = entry.path();
 			let _ = xcopy(
@@ -115,12 +117,15 @@ fn xcopy(
 		return Ok(());
 	}
 	if source_path.is_file() {
-		let _file_name = source_path.file_name().unwrap().to_str().unwrap();
 		return handler(
 			source_path.to_str().unwrap(),
 			destination_path.to_str().unwrap(),
 		);
 	}
+	println!(
+		"[WARN] 不明なファイルです。[{}]",
+		source_path.to_str().unwrap()
+	);
 	return Ok(());
 }
 
@@ -131,5 +136,8 @@ fn main() {
 		println!("path?");
 		return;
 	}
-	let _ = xcopy(&args[1], &args[2], &copy_file);
+	let result = xcopy(&args[1], &args[2], &copy_file);
+	if result.is_err() {
+		println!("[ERROR] <main()> {}", result.err().unwrap());
+	}
 }
