96a0bbb30adf4a661680b3ee4f184ae9176636ca
Irisawa Masaru <mass10.github@gmail.com>
2020-04-15 00:32:35 +0900

    wip

--- code/commandline-arguments/main.rs
+++ code/commandline-arguments/main.rs
@@ -1,14 +1,12 @@
-use std::env;
-
 fn main() {
 
-	let args: Vec<String> = env::args().collect();
+	let args: Vec<String> = std::env::args().collect();
 	if args.len() < 2 {
 		println!("what?");
 		return;
 	}
 
 	for e in &args[1..] {
-	    println!("{:?}", e);
+	    println!("[{}]", e);
 	}
 }
