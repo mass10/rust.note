ffcbd5eea8d3c130da0e130fe97bed7a72f8d0e2
Irisawa Masaru <mass10.github@gmail.com>
2020-04-26 15:47:31 +0900

    wip

--- code/time/elapsed/src/main.rs
+++ code/time/elapsed/src/main.rs
@@ -1,11 +1,14 @@
 use std::time;
 use std::thread;
 
-trait formatting {
+trait Formatting {
+	/// 経過時間の文字列表現を得る
 	fn to_string(&self) -> String;
+	/// 経過時間の文字列表現を得る
+	fn to_string2(&self) -> String;
 }
 
-impl formatting for time::Duration {
+impl Formatting for std::time::Duration {
 	fn to_string(&self) -> String {
 		let mut sec = self.as_secs();
 		let mut min = 0;
@@ -21,6 +24,26 @@ impl formatting for time::Duration {
 		let s = format!("{:02}:{:02}:{:02}", hour, min, sec);
 		return s;
 	}
+	fn to_string2(&self) -> String {
+		let mut millis = self.as_millis();
+		let mut sec = 0;
+		let mut min = 0;
+		let mut hour = 0;
+		while 1000 <= millis {
+			sec += 1;
+			millis -= 1000;
+		}
+		while 60 <= sec {
+			min += 1;
+			sec -= 60;
+		}
+		while 60 <= min {
+			hour += 1;
+			min -= 60;
+		}
+		let s = format!("{:02}:{:02}:{:02}:{:03}", hour, min, sec, millis);
+		return s;
+	}
 }
 
 fn duration(left: time::Instant, right: time::Instant) {
@@ -29,6 +52,7 @@ fn duration(left: time::Instant, right: time::Instant) {
 
 	println!("{:?}", elapsed);
 	println!("{}", elapsed.to_string());
+	println!("{}", elapsed.to_string2());
 	println!("{}", elapsed.subsec_nanos());
 }
 
@@ -36,7 +60,7 @@ fn main() {
 
 	let time1 = time::Instant::now();
 
-	thread::sleep(time::Duration::new(2, 234));
+	thread::sleep(time::Duration::new(2, 123456789));
 
 	let time2 = time::Instant::now();
 
