c03f8e8b5699701a0ffd411dae568f0e408bf2a4
Irisawa Masaru <mass10.github@gmail.com>
2020-05-17 04:28:36 +0900

    wip

---
+++ code/const/src/constants/system.rs
@@ -0,0 +1,12 @@
+#[allow(unused)]
+pub const WIN: &str = "Microsoft Windows";
+#[allow(unused)]
+pub const UBUNTU: &str = "Ubuntu";
+#[allow(unused)]
+pub const KALI: &str = "Kali Linux";
+#[allow(unused)]
+pub const TSURUGI: &str = "Tsurugi Linux";
+#[allow(unused)]
+pub const REDHAT: &str = "Red Hat Enterprise Linux";
+#[allow(unused)]
+pub const DEBIAN: &str = "Debian";
---
+++ code/const/src/main.rs
@@ -0,0 +1,20 @@
+/// 消費税率の定義
+const TAX: f32 = 0.1;
+
+/// 文字列リテラルの定義
+const GREETING: &str = "Hello, World!";
+
+/// 定数モジュールの参照
+mod constants;
+
+fn main() {
+
+	// 消費税率の参照
+	println!("Hello, Tax !{}", TAX);
+
+	// 文字列リテラルの参照
+	println!("{}", GREETING);
+
+	// 定数モジュールから定数の参照
+	println!("{}", constants::system::KALI);
+}
