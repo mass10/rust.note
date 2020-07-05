af5d1a04a798f07dac4cdd31888e1325d4240bdb
Irisawa Masaru <mass10.github@gmail.com>
2020-07-05 18:29:29 +0900

    wip

--- code/sleep/src/main.rs
+++ code/sleep/src/main.rs
@@ -14,12 +14,15 @@ fn sleep(secs: u64) {
 /// エントリーポイント
 fn main() {
 	// コマンドライン引数をひとつとる
-	let args: Vec<String> = std::env::args().collect();
-	if args.len() < 2 {
+	let args: Vec<String> = std::env::args().skip(1).collect();
+	if args.len() < 1 {
 		return;
 	}
 	// 数値変換
-	let wait = parse_uint(&args[1]);
+	let wait = parse_uint(&args[0]);
+	if wait == 0 {
+		return;
+	}
 	// スリープ
 	sleep(wait);
 }
