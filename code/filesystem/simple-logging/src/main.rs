8489a812ddf27c002a744b15ca0a178e9b5778f2
Irisawa Masaru <mass10.github@gmail.com>
2020-07-07 07:42:31 +0900

    wip

--- code/filesystem/simple-logging/src/main.rs
+++ code/filesystem/simple-logging/src/main.rs
@@ -1,25 +1,22 @@
 use std::io::Write;
 
 fn trunc_and_write() {
-
 	// always create
 	let mut f = std::fs::File::create("main.log").unwrap();
 	f.write_all(b"trunc.\n").unwrap();
 }
 
-fn append() {
-
+fn append(text: &str) {
 	// create or append
 	let mut f = std::fs::OpenOptions::new().create(true).append(true).open("main.log").unwrap();
-	f.write_all(b"test.\n").unwrap();
+	f.write_all(text.as_bytes()).unwrap();
+	f.write_all(b"\n").unwrap();
 }
 
 fn main() {
-
+	append("000");
 	trunc_and_write();
-	append();
-	append();
-	append();
-	append();
-	append();
+	append("aaa");
+	append("bbb");
+	append("ccc");
 }
