3cab594349001a894e8616a4a80f653f79787e0a
Irisawa Masaru <mass10.github@gmail.com>
2020-07-27 10:25:05 +0900

    wip

--- code/functions/main.rs
+++ code/functions/main.rs
@@ -2,13 +2,13 @@ fn test1(s: &str) {
 	println!("{}", s)
 }
 
+/// 文字列を返す関数
 fn create_string() -> String {
-
 	return String::from("Hello, Real Rust World!");
 }
 
+/// エントリーポイント
 fn main() {
-
 	test1("Hello, Real Rust World!");
 	println!("{}", create_string());
 	println!("{}", create_string().as_str());
