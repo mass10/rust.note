f43f4cdb5cdd1a7c97208828b695de86142ebd33
Irisawa Masaru <mass10.github@gmail.com>
2020-08-21 23:40:24 +0900

    wip

--- code/keyevents/src/main.rs
+++ code/keyevents/src/main.rs
@@ -1,51 +1,61 @@
 #[macro_use]
 extern crate crossterm;
-use crossterm::cursor;
-use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
-use crossterm::style::Print;
-use crossterm::terminal::{enable_raw_mode, Clear, ClearType};
-use std::io::{stdout, Write};
 
-fn main() {
-	let mut stdout = stdout();
-	enable_raw_mode().unwrap();
+/// 画面を消去します。
+fn cls() -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
+	use crossterm::terminal::{Clear, ClearType};
+	use std::io::Write;
+
+	let mut stdout = std::io::stdout();
 	// 画面を消去します。
-	execute!(stdout, Clear(ClearType::All)).unwrap();
+	execute!(stdout, Clear(ClearType::All))?;
 	// 左上 [0, 0] へ移動します。
-	execute!(stdout, cursor::MoveTo(0, 0)).unwrap();
+	execute!(stdout, crossterm::cursor::MoveTo(0, 0))?;
+	return Ok(());
+}
 
-	loop {
-		// 次のキー操作を待ちます。
-		let key_press = read().unwrap();
-		match key_press {
-			Event::Key(KeyEvent {
-				code: KeyCode::Char('l'),
-				modifiers: KeyModifiers::CONTROL,
-			}) => execute!(stdout, Clear(ClearType::All), Print("")).unwrap(),
-			Event::Key(KeyEvent {
-				code: KeyCode::Char('c'),
-				modifiers: KeyModifiers::ALT,
-			}) => break,
-			// [Ctrl][C] to quit.
-			Event::Key(KeyEvent {
-				code: KeyCode::Char('c'),
-				modifiers: KeyModifiers::CONTROL,
-			}) => break,
-			// [Q] to quit.
-			Event::Key(KeyEvent {
-				code: KeyCode::Char('q'),
-				modifiers: KeyModifiers::NONE,
-			}) => break,
-			// Else
-			e => {
-				// 画面を消去します。
-				execute!(stdout, Clear(ClearType::All)).unwrap();
-				// 左上 [0, 0] へ移動します。
-				execute!(stdout, cursor::MoveTo(0, 0)).unwrap();
-				print!("> ");
-				print!("KEY: {:?}", e);
-				std::io::stdout().flush().unwrap();
-			}
+/// キーイベント
+fn on_key_down(key: crossterm::event::Event) -> std::result::Result<bool, std::boxed::Box<dyn std::error::Error>> {
+	use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+	use crossterm::style::Print;
+	use crossterm::terminal::{Clear, ClearType};
+	use std::io::Write;
+
+	match key {
+		Event::Key(KeyEvent { code: KeyCode::Char('l'), modifiers: KeyModifiers::CONTROL }) => {
+			execute!(std::io::stdout(), Clear(ClearType::All), Print(""))?;
+			return Ok(true);
+		}
+		Event::Key(KeyEvent { code: KeyCode::Char('c'), modifiers: KeyModifiers::ALT }) => return Ok(false),
+		// [Ctrl][C] to quit.
+		Event::Key(KeyEvent { code: KeyCode::Char('c'), modifiers: KeyModifiers::CONTROL }) => return Ok(false),
+		// [Q] to quit.
+		Event::Key(KeyEvent { code: KeyCode::Char('q'), modifiers: KeyModifiers::NONE }) => return Ok(false),
+		// Else
+		e => {
+			// 画面を消去します。
+			cls()?;
+			// キーを表示します。
+			print!("> KEY: {:?}", e);
+			std::io::stdout().flush()?;
+			return Ok(true);
 		}
 	}
+}
+
+fn main() -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
+	// RAW MODE
+	crossterm::terminal::enable_raw_mode()?;
+	// CLS
+	cls()?;
+	// キーイベントループ
+	loop {
+		// 次のキー操作を待ちます。
+		let key = crossterm::event::read()?;
+		if !on_key_down(key)? {
+			break;
+		}
+	}
+
+	return Ok(());
 }
