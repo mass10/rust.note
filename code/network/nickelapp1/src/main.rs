7ec8995b6e9e5061aae19d62ed78f3d3dbee47d1
Irisawa Masaru <mass10.github@gmail.com>
2020-09-04 15:46:17 +0900

    wip

--- code/network/nickelapp1/src/main.rs
+++ code/network/nickelapp1/src/main.rs
@@ -1,12 +1,16 @@
-#[macro_use] extern crate nickel;
+#[macro_use]
+extern crate nickel;
 
 use nickel::Nickel;
 
 fn main() {
-
 	let mut server = Nickel::new();
 
 	server.utilize(router! {
+		// 有効なルーティング。完全一致のようだ。
+		get "/json1" => |_req, _res| {
+			format!("あいうえお")
+		}
 		// 全ての GET をひっかけるようだ。
 		get "**" => |_req, _res| {
 			format!("{:?}: [/] Hello world!", std::thread::current().id())
@@ -19,6 +23,8 @@ fn main() {
 		post "test2" => |_req, _res| {
 			format!("{:?}: [test2]: accepted post data.", std::thread::current().id())
 		}
+
+
 	});
 
 	let _result = server.listen("127.0.0.1:6767");
