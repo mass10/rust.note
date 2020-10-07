32d6ae04de7b445bb7a09918710c0f2c3d15b73c
Irisawa Masaru <mass10.github@gmail.com>
2020-10-07 22:48:07 +0900

    Update main.rs

--- code/digest/md5/src/main.rs
+++ code/digest/md5/src/main.rs
@@ -9,7 +9,27 @@ fn test(s: &str) {
 	println!("[TRACE] [{}] >> [{}] (MD5)", s, digest);
 }
 
-fn main() {
+fn dump_file_md5(path: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
+	use std::io::Read;
+
+	// ファイルを開きます。
+	let mut file = std::fs::File::open(path)?;
+
+	// 読み取り用バッファ
+	let mut buffer = std::vec::Vec::new();
+
+	// ファイル全体を読み込み
+	file.read_to_end(&mut buffer)?;
+
+	// MD5 を計算
+	let response = md5::compute(buffer);
+	println!("{:x}", response);
+
+	return Ok(());
+}
+
+#[allow(unused)]
+fn main_bak() {
 	test("Hello, world!");
 	test("コンニチハ！");
 	test("https://www.nittsu.co.jp");
@@ -18,4 +38,20 @@ fn main() {
 	test("https://www.nissan.co.jp");
 	test("https://www.marubeni.com");
 	test("https://www.hd.eneos.co.jp");
+}
+
+/// エントリーポイント
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+	// コマンドライン引数を先頭から一つだけ取り出します。
+	let args: Vec<String> = std::env::args().skip(1).collect();
+	if args.len() == 0 {
+		println!("path?");
+		return Ok(());
+	}
+	let path = &args[0];
+
+	// ファイルの MD5 を出力します。
+	dump_file_md5(path)?;
+
+	return Ok(());
 }
