3b721783ae6cec46adf623adfd79b6e0044ea262
Irisawa Masaru <mass10.github@gmail.com>
2020-12-14 22:42:35 +0900

    wip

--- code/vector/target/debug/deps/vector.d
+++
@@ -1,5 +0,0 @@
-C:\Users\irisa\workspace\rust.note\code\vector\target\debug\deps\vector.exe: main.rs
-
-C:\Users\irisa\workspace\rust.note\code\vector\target\debug\deps\vector.d: main.rs
-
-main.rs:
--- code/vector/target/release/vector.d
+++
@@ -1,1 +0,0 @@
-C:\Users\irisa\workspace\rust.note\code\vector\target\release\vector.exe: C:\Users\irisa\workspace\rust.note\code\vector\main.rs
