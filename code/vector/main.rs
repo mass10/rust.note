40dad24aa99c3a255f59bc61797e742443e7d65f
Irisawa Masaru <mass10.github@gmail.com>
2020-12-16 09:47:27 +0900

    wip

--- code/vector/main.rs
+++ code/vector/main.rs
@@ -1,3 +1,27 @@
+#[derive(Debug, Clone)]
+pub struct ApplicationError {
+	pub description: String,
+}
+
+impl ApplicationError {
+	#[allow(unused)]
+	pub fn new(description: String) -> ApplicationError {
+		return ApplicationError { description: description };
+	}
+}
+
+impl std::fmt::Display for ApplicationError {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+		write!(f, "{}", self.description)
+	}
+}
+
+impl std::error::Error for ApplicationError {
+	fn description(&self) -> &str {
+		&self.description
+	}
+}
+
 ///
 /// データベースクラス
 ///
@@ -13,27 +37,28 @@ impl DatabaseAccess {
 
 	/// 生徒名簿を抽出します。
 	/// Heap 上のオブジェクトへの参照を返します。
-	pub fn enum_students(&self, classroom: &str) -> Box<Vec<String>> {
+	pub fn enum_students_boxed_type(&self, classroom: &str) -> Box<Vec<Box<String>>> {
 		if classroom == "" {}
-		let mut students: Vec<String> = vec![];
-		students.push(String::from("jimi.hendrix@gmail.com"));
-		students.push(String::from("paul.kossoff@gmail.com"));
-		students.push(String::from("freddie.mercury@gmail.com"));
-		students.push(String::from("billy.preston@gmail.com"));
-		students.push(String::from("steve.marriot@gmail.com"));
+		let mut students: Vec<Box<String>> = vec![];
+		students.push(Box::new(String::from("jimi.hendrix@gmail.com")));
+		students.push(Box::new(String::from("paul.kossoff@gmail.com")));
+		students.push(Box::new(String::from("freddie.mercury@gmail.com")));
+		students.push(Box::new(String::from("billy.preston@gmail.com")));
+		students.push(Box::new(String::from("steve.marriot@gmail.com")));
 		return Box::new(students);
 	}
 
-	pub fn validate_registration_time(&self, students: &Vec<String>) -> bool {
+	pub fn validate_registration_time(&self, students: &Vec<Box<String>>) -> std::result::Result<(), Box<dyn std::error::Error>> {
 		for e in students.into_iter() {
 			if !e.contains("@") {
-				return false;
+				let error = ApplicationError::new("何らかのエラー".to_string());
+				return Err(Box::new(error));
 			}
 		}
-		return true;
+		return Ok(());
 	}
 
-	pub fn validate_registration_email(&self, students: &Vec<String>) -> bool {
+	pub fn validate_registration_email(&self, students: &Vec<Box<String>>) -> bool {
 		for e in students.into_iter() {
 			if e.len() == 0 {
 				return false;
@@ -43,12 +68,21 @@ impl DatabaseAccess {
 	}
 }
 
+/// Vec に破壊的な変更を加える何らかの操作
+fn modify_vector_entries(students: &mut Vec<Box<String>>) {
+	for e in students {
+		if e.contains("jimi") {
+			e.extend(" ★".chars());
+		}
+	}
+}
+
 ///
 /// クラスルームのクラス
 ///
 struct Classroom {
 	_x: i32,
-	students: Vec<String>,
+	students: Vec<Box<String>>,
 	classroom: String,
 }
 
@@ -66,32 +100,27 @@ impl Classroom {
 
 	/// オブジェクトの初期化を行います。
 	fn initialize(&mut self) {
-		// 内部リストを初期化して
-		self.students.clear();
-
 		// データベースクラス
 		let dba = DatabaseAccess::new();
 
 		//生徒名簿を抽出
-		let rows = dba.enum_students(&self.classroom);
-
-		// 内部リストへ保管します。
-		for e in rows.into_iter() {
-			self.students.push(e);
-		}
+		self.students = dba.enum_students_boxed_type(&self.classroom).to_vec();
 	}
 
+	#[allow(unused)]
 	/// 生徒名簿を返します。返却されるオブジェクトは複製です。
-	pub fn enum_students_clone(&self) -> Vec<String> {
+	pub fn enum_students_clone(&self) -> Vec<Box<String>> {
 		return self.students.clone();
 	}
 
+	#[allow(unused)]
 	/// 生徒名簿を返します。返却されるオブジェクトは参照です。
-	pub fn enum_students_ref(&self) -> &Vec<String> {
-		return &self.students;
+	pub fn enum_students_ref(&mut self) -> &mut Vec<Box<String>> {
+		return self.students.as_mut();
 	}
 }
 
+#[allow(unused)]
 fn test(v: &Vec<i64>) {
 	print!("[TRACE] ");
 	for (i, e) in v.iter().enumerate() {
@@ -103,7 +132,8 @@ fn test(v: &Vec<i64>) {
 	println!("");
 }
 
-fn test01() {
+#[allow(unused)]
+fn test00() {
 	let v = vec!["こんにちは", "Real", "World", "Rust"];
 	println!("{:?}", v);
 
@@ -115,37 +145,72 @@ fn test01() {
 	println!("{:?}", v);
 
 	let v = vec![1, 3, -89, 67, 10092, -1029, -12, -10];
-	println!("3つめの要素は {0} です。", v[2]);
-
-	{
-		let classroom = Classroom::new("A");
-		println!("{:?}", classroom.enum_students_clone());
-		println!("{:?}", classroom.enum_students_clone());
-		println!("{:?}", classroom.enum_students_clone());
-		println!("{:?}", classroom.enum_students_clone());
-	}
-
-	{
-		// Classroom の生徒名簿を参照します。
-		let classroom = Classroom::new("A");
-		let students = classroom.enum_students_ref();
-
-		// 検証っぽい何かの処理を行います。
-		let db = DatabaseAccess::new();
-		// 参照で見せるだけなら何も制限はありません。
-		db.validate_registration_time(students);
-		// 参照で見せるだけなら何も制限はありません。
-		db.validate_registration_email(students);
-
-		for student in students {
-			println!("[TRACE] {}", student);
-		}
-	}
+	println!("[TRACE] 3つめの要素は {0} です。", v[2]);
 }
 
-fn test02() {}
+#[allow(unused)]
+fn test01() -> std::result::Result<(), Box<dyn std::error::Error>> {
+	// Classroom を参照します。
+	let classroom = Classroom::new("A");
 
-fn main() {
-	test01();
-	test02();
+	// 生徒名簿
+	let students = classroom.enum_students_clone();
+
+	// データベース接続
+	let db = DatabaseAccess::new();
+
+	// Vec を読み取り専用アクセス(Rust では「借用」、よく「参照」と言われる仕組み)
+	db.validate_registration_time(&students)?;
+	db.validate_registration_time(&students)?;
+	db.validate_registration_time(&students)?;
+
+	for e in classroom.enum_students_clone() {
+		println!("[TRACE] {}", e);
+	}
+
+	return Ok(());
+}
+
+#[allow(unused)]
+fn test02() -> std::result::Result<(), Box<dyn std::error::Error>> {
+	// Classroom を参照します。
+	let mut classroom = Classroom::new("A");
+
+	// 生徒名簿
+	let students = classroom.enum_students_ref();
+
+	// データベース接続
+	let db = DatabaseAccess::new();
+
+	// 何らかのバリデーション。読み取り専用の「借用」(=参照)アクセス。
+	db.validate_registration_time(students)?;
+
+	// 要素に変更を加える操作を呼びだします。
+	modify_vector_entries(students);
+
+	// 何らかのバリデーション。読み取り専用の「借用」(=参照)アクセス。
+	db.validate_registration_email(students);
+
+	for e in students {
+		println!("[TRACE] {}", e);
+	}
+
+	return Ok(());
+}
+
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+	let args: Vec<String> = std::env::args().skip(1).collect();
+	let _request = if 0 < args.len() { args[0].as_str() } else { "" };
+
+	if false {
+		test00();
+	}
+	if false {
+		test01()?;
+	}
+	if true {
+		test02()?;
+	}
+
+	return Ok(());
 }
