ed0ccacdd0efe5391123abb9245e0468d67632e9
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-01-01 00:05:03 +0900

    Update main.rs

--- code/multithreading/example0/src/main.rs
+++ code/multithreading/example0/src/main.rs
@@ -2,15 +2,42 @@ use std::thread;
 use std::time;
 
 fn main() {
-
 	println!("### START ###");
 
-	let handle = thread::spawn(|| {
+	let (tx, rx): (std::sync::mpsc::Sender<String>, std::sync::mpsc::Receiver<String>) = std::sync::mpsc::channel();
+
+	let handle = thread::spawn(move || {
+
 		println!("$$$ begin thread $$$");
+
+		let result = tx.send("START".to_string());
+		if result.is_err() {
+			let error = result.err().unwrap();
+			println!("[ERROR] Unknown error. reason: [{}]", error);
+		}
+
 		std::thread::sleep(time::Duration::from_millis(1000 * 5));
+
 		println!("--- exit thread ---");
-		return 999;
+
+		let result = tx.send("END".to_string());
+		if result.is_err() {
+			let error = result.err().unwrap();
+			println!("[ERROR] Unknown error. reason: [{}]", error);
+		}
+
+		return "".to_string();
 	});
+
+	{
+		let result = rx.recv();
+		if result.is_err() {
+			println!("[ERROR] reason: {}", result.err().unwrap());
+		} else {
+			let result = result.ok();
+			println!("[TRACE] result: [{:?}]", result);
+		}
+	}
 
 	let result = handle.join();
 	match result {
