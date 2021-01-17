d88e36cd551328287dc49b68f38cbfd5a834e468
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-01-17 09:53:15 +0900

    Update main.rs

--- code/match/eight-days-a-week/src/main.rs
+++ code/match/eight-days-a-week/src/main.rs
@@ -1,20 +1,22 @@
+/// 曜日 `n` の表示用テキストを返します。
 fn name_of_day(n: i64) -> &'static str {
 
-    match n {
-        0 => "Sunday",
-        1 => "Monday",
-        2 => "Tue",
-        3 => "Wed",
-        4 => "Thu",
-        5 => "Fri",
-        6 => "Sat",
-        _ => ""
-    }
+	match n {
+		0 => "Sunday",
+		1 => "Monday",
+		2 => "Tue",
+		3 => "Wed",
+		4 => "Thu",
+		5 => "Fri",
+		6 => "Sat",
+		_ => "Eight Days A Week ?"
+	}
 }
 
+/// エントリーポイント
 fn main() {
 
-    for n in 0..8 {
-        println!("{} => \"{}\"", n, name_of_day(n));
-    }
+	for n in 0..8 {
+		println!("{} => \"{}\"", n, name_of_day(n));
+	}
 }
