25a05e09eb57683894e8e52c975acad3bb266c05
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-01-20 23:29:21 +0900

    Update main.rs

--- code/struct/reference/src/main.rs
+++ code/struct/reference/src/main.rs
@@ -2,26 +2,26 @@
 /// 構造体 A
 ///
 struct A {
-    /// 名前
-    name: String
+	/// 名前
+	name: String,
 }
 
 /// オブジェクト `A` の参照を受け取ります。(borrowed)
 /// * `a` ... type `A`
 fn dump(a: &A) {
+	println!("{}", a.name);
 
-    println!("{}", a.name);
-
-    // CANNOT MODIFY FIELD VALUE OF BORROWED OBJECT.
-    // a.name = "".to_string();
+	// CANNOT MODIFY FIELD VALUE OF BORROWED OBJECT.
+	// a.name = "".to_string();
 }
 
 /// エントリーポイント
 fn main() {
+	// 構造体 `A` の immutable な実体
+	let a = A { name: "Hahaha".to_string() };
 
-    let a = A{name: "Hahaha".to_string()};
+	// オブジェクトをダンプ
+	dump(&a);
 
-    dump(&a);
-
-    println!("Ok.");
+	println!("Ok.");
 }
