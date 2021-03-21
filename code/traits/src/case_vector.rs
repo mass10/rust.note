2d744c99d302d72abbb58ee1668b5cf39d5b3fc6
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-03-21 14:36:31 +0900

    Create case_vector.rs

---
+++ code/traits/src/case_vector.rs
@@ -0,0 +1,29 @@
+type StringVectorType = std::vec::Vec<std::string::String>;
+
+trait StringVectorTrait<StringVectorType> {
+	fn at(&self, index: usize) -> String;
+}
+
+impl StringVectorTrait<StringVectorType> for StringVectorType {
+	fn at(&self, index: usize) -> String {
+		if self.len() <= index {
+			return "".to_string();
+		}
+		return self[index].to_string();
+	}
+}
+
+pub fn execute() {
+	println!("### vec をテストします ###");
+
+	let v = vec![
+		String::from(""),
+		String::from("bbbbbb"),
+		String::from("ジミヘン"),
+	];
+
+	println!("{}", v.at(0));
+	println!("{}", v.at(2));
+	println!("{}", v.at(99));
+	println!();
+}
