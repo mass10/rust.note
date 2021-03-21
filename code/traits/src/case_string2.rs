0650d14e597d7f1be08fb4263829a68e7ab5ef17
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-03-21 14:36:19 +0900

    Create case_string2.rs

---
+++ code/traits/src/case_string2.rs
@@ -0,0 +1,46 @@
+/// トレイトの宣言
+trait TestableString<T> {
+	/// この T が「アルファベットである」とみなすことができる場合に true を返します。
+	fn is_alpha(&self) -> bool;
+}
+
+impl TestableString<&str> for &str {
+	fn is_alpha(&self) -> bool {
+		if self.len() == 0 {
+			return false;
+		}
+		for ch in self.chars() {
+			// 全角をはじく
+			if ch.is_ascii_alphabetic() == false {
+				return false;
+			}
+			// 大文字 "A"
+			if ch.is_uppercase() {
+				continue;
+			}
+			// 小文字 "a"
+			if ch.is_lowercase() {
+				continue;
+			}
+			return false;
+		}
+		return true;
+	}
+}
+
+fn test_str(n: &str) {
+	let result = ::myutil::result(n.is_alpha());
+	println!("{:?}.slphabet() -> [{}]", n, result);
+}
+
+pub fn execute() {
+	println!("### 文字列をテストします ###");
+	test_str("_");
+	test_str("あいうえお");
+	test_str("hhHgsAYTWhnxbA");
+	test_str("892137981240");
+	test_str("_");
+	test_str("Ｊｉｍｉ");
+	test_str("Ａ");
+	println!();
+}
