a78518591706d86636c36dc17f062ed48536211e
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-03-21 15:02:37 +0900

    Update main.rs

--- code/traits/src/main.rs
+++ code/traits/src/main.rs
@@ -5,9 +5,18 @@ mod case_vector;
 mod myutil;
 
 fn main() {
+
+	// 数値に trait を実装してみる
 	case_integral::execute();
+
+	// 文字列に trait を実装してみる
 	case_string::execute();
+
+	// 文字列に trait を実装してみる
 	case_string2::execute();
+
+	// Vec に trait を実装してみる
 	case_vector::execute();
+
 	println!("Ok.");
 }
