9076c60a267266c4433dffb465403dc3d2ac1ed2
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-03-22 22:39:31 +0900

    Update main.rs

--- code/archive-zip/src/main.rs
+++ code/archive-zip/src/main.rs
@@ -1,20 +1,45 @@
 use std::io::Read;
 
+use chrono::{Datelike, Timelike};
+
 // use std::{net::ToSocketAddrs, ops::Add};
+
+#[allow(unused)]
+fn get_file_name(path: &str) -> String {
+	let unknown = std::path::Path::new(path);
+	return unknown.file_name().unwrap().to_str().unwrap().to_string();
+}
 
 /// ディレクトリまたはファイルを削除します。
 fn unlink(path: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
+	if path == "" {
+		return Ok(());
+	}
+	if path == "/" {
+		return Ok(());
+	}
+	if path == "C:" {
+		return Ok(());
+	}
+	if path == "C:\\" {
+		return Ok(());
+	}
+	if path == "C:\\Windows" {
+		return Ok(());
+	}
+
 	let e = std::path::Path::new(path);
 	if !e.exists() {
 		return Ok(());
 	}
+
 	if e.is_dir() {
-		println!("[TRACE] {} を削除中...", path);
+		println!("[TRACE] {} を削除", path);
 		std::fs::remove_dir_all(path)?;
 		return Ok(());
 	}
 	if e.is_file() {
-		println!("[TRACE] {} を削除中...", path);
+		println!("[TRACE] {} を削除", path);
 		std::fs::remove_file(path)?;
 		return Ok(());
 	}
@@ -25,41 +50,90 @@ struct MyArchiver {
 	// archiver: Option<zip::ZipWriter<std::fs::File>>,
 }
 
+fn build_path(base_name: &str, name: &str) -> String {
+	let unknown = std::path::Path::new(base_name);
+	let path_name = unknown.join(name);
+	return path_name.to_str().unwrap().to_string();
+}
+
 impl MyArchiver {
 	/// 作成
 	pub fn new() -> MyArchiver {
 		return MyArchiver {};
 	}
 
-	fn append_entry(&mut self, archiver: &mut zip::ZipWriter<std::fs::File>, path: &str) -> Result<(), Box<dyn std::error::Error>> {
-		#[allow(unused)]
+	fn append_entry(&mut self, archiver: &mut zip::ZipWriter<std::fs::File>, base_name: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+		// #[allow(unused)]
 		use std::io::Write;
+
+		// println!("[TRACE] BASENAME: [{}]", base_name);
 
 		let unknown = std::path::Path::new(path);
 		if !unknown.exists() {
 			return Ok(());
 		} else if unknown.is_dir() {
-			println!("[TRACE] サブディレクトリ {} を追加中...", path);
+			// println!("[TRACE] ディレクトリ {} を検索中...", path);
 
-			let options = zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Stored);
-			archiver.add_directory(path, options)?;
-			let reader = unknown.read_dir()?;
-			for e in reader {
-				let entry = e.unwrap();
-				self.append_entry(archiver, entry.path().to_str().unwrap())?;
+			let name = unknown.file_name().unwrap().to_str().unwrap();
+
+			// 内部ディレクトリエントリーを作成
+			if base_name != "" {
+				// build_path(base_name, name);
+				let options = zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Stored);
+				archiver.add_directory(build_path(base_name, name), options)?;
+			}
+
+			let it = std::fs::read_dir(path)?;
+			for e in it {
+				let entry = e?;
+
+				// println!("[TRACE] CHILD: {:?}", entry.path());
+
+				// ディレクトリ／ファイルの名前
+				// let name = entry.file_name().to_str().unwrap().to_string();
+				// base_name からの内部相対パスを生成
+				let sub_dir_name = build_path(base_name, &name);
+
+				let path_name = entry.path();
+				let path_name = path_name.to_str().unwrap();
+
+				// println!("[TRACE] (found {})", path_name);
+
+				self.append_entry(archiver, &sub_dir_name, path_name)?;
 			}
 		} else if unknown.is_file() {
-			println!("[TRACE] ファイル {} を追加中...", path);
+			let full_path = unknown.to_str().unwrap();
+			let name = unknown.file_name().unwrap().to_str().unwrap();
+
+			let relative_path = build_path(base_name, name);
+			println!("[TRACE] ファイル {} ({}) を追加中...", &relative_path, full_path);
 
 			// zip 内におけるルートからの絶対パス "/a/b/c/d.txt"
-			let relative_path = unknown.to_str().unwrap();
-			println!("[TRACE] >> ファイル {}", relative_path);
+			// println!("[TRACE] >> ファイル {}", unknown.to_str().unwrap());
+
+			let meta = unknown.metadata()?;
+			let last_modified = meta.modified()?;
+			let chrono_timestamp = chrono::DateTime::<chrono::Local>::from(last_modified);
 
 			// ファイルをアーカイブ
 			let options = zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Stored);
-			archiver.start_file(relative_path, options)?;
 
-			let mut stream = std::fs::File::open(relative_path)?;
+			// 最終更新日時
+			let last_modified_time = zip::DateTime::from_date_and_time(
+				chrono_timestamp.year() as u16,
+				chrono_timestamp.month() as u8,
+				chrono_timestamp.day() as u8,
+				chrono_timestamp.hour() as u8,
+				chrono_timestamp.minute() as u8,
+				chrono_timestamp.second() as u8,
+			)
+			.unwrap();
+			let options = options.last_modified_time(last_modified_time);
+
+			// let options = options.last_modified_time(last_modified);
+			archiver.start_file(&relative_path, options)?;
+
+			let mut stream = std::fs::File::open(full_path)?;
 			loop {
 				let mut buffer = [0; 1000];
 				let bytes_read = stream.read(&mut buffer)?;
@@ -67,12 +141,12 @@ impl MyArchiver {
 					break;
 				}
 				let write_buffer = &buffer[..bytes_read];
-				let bytes_written = archiver.write(&write_buffer)?;
-				println!("[TRACE] (read {} bytes, written {} bytres...)", bytes_read, bytes_written);
+				archiver.write(&write_buffer)?;
 			}
 			// archiver.write(b"Hello, World!")?;
 			// archiver.finish()?;
 		}
+
 		return Ok(());
 	}
 
@@ -84,20 +158,23 @@ impl MyArchiver {
 	/// ディレクトリをアーカイブします。
 	pub fn archive(&mut self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
 		println!("[TRACE] {} をアーカイブ中...", path);
-		// use std::io::Write;
 
 		// ファイル名を生成
-		let archive_path = MyArchiver::create_zip_name(path);
+		let archive_path_name = MyArchiver::create_zip_name(path);
+
+		// 起点となるディレクトリの名前
+		// let base_name = get_file_name(path);
+		let base_name = "";
 
 		// .zip ファイルがあれば削除
-		unlink(&archive_path)?;
+		unlink(&archive_path_name)?;
 
 		// アーカイバーの初期化
-		let w = std::fs::File::create(archive_path)?;
+		let w = std::fs::File::create(archive_path_name)?;
 		let mut archiver = zip::ZipWriter::new(w);
 
 		// ここから走査
-		self.append_entry(&mut archiver, path)?;
+		self.append_entry(&mut archiver, &base_name, path)?;
 
 		// アーカイバーを閉じます。
 		archiver.finish()?;
@@ -119,17 +196,15 @@ fn main() {
 		return;
 	}
 
-	let path = &args[0];
-
-	// unlink("src.zip")?;
-
-	// zip
-	let mut ar = MyArchiver::new();
-	let result = ar.archive(&path);
-	if result.is_err() {
-		let error = result.err();
-		println!("[ERROR] {}", error.unwrap());
-		return;
+	for path in &args {
+		// それぞれを、それぞれの zip にアーカイブします。
+		let mut ar = MyArchiver::new();
+		let result = ar.archive(&path);
+		if result.is_err() {
+			let error = result.err();
+			println!("[ERROR] {}", error.unwrap());
+			return;
+		}
 	}
 
 	println!("Ok.");
