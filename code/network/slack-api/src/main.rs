0e5746afc407c9761e7c6bc21ae994fd56aecbb4
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-03-31 04:52:23 +0900

    Update main.rs

--- code/network/slack-api/src/main.rs
+++ code/network/slack-api/src/main.rs
@@ -1,190 +1,207 @@
 extern crate reqwest;
 
+/// テキストファイル全体を読み込んで返します。
 ///
-/// 各種ユーティリティを提供します。
+/// ### Arguments
+/// * `path` ファイルへのパス
 ///
-mod util {
+/// ### Returns
+/// ファイル全体の内容を返します。
+pub fn read_text_file(path: &str) -> std::result::Result<String, Box<dyn std::error::Error>> {
+	use std::io::Read;
 
-	/// テキストファイル全体を読み込んで返します。
-	///
-	/// ### Arguments
-	/// * `path` ファイルへのパス
-	///
-	/// ### Returns
-	/// ファイル全体の内容を返します。
-	pub fn read_text_file(path: &str) -> std::result::Result<String, Box<dyn std::error::Error>> {
-		use std::io::Read;
+	let mut file = std::fs::File::open(path).unwrap();
+	let mut s = String::new();
+	file.read_to_string(&mut s)?;
+	return Ok(s);
+}
 
-		let mut file = std::fs::File::open(path).unwrap();
-		let mut s = String::new();
-		file.read_to_string(&mut s)?;
-		return Ok(s);
+/// JSON をパースします。
+///
+/// ### Arguments
+/// * `json_text` JSON 文字列
+///
+/// ### Returns
+/// `serde_json::Value`
+pub fn parse_json_to_value(json_text: &str) -> std::option::Option<serde_json::Value> {
+	let result = serde_json::from_str::<serde_json::Value>(json_text);
+	if result.is_err() {
+		return None;
 	}
+	return Some(result.ok().unwrap());
+}
 
-	/// JSON をパースします。
-	///
-	/// ### Arguments
-	/// * `json_text` JSON 文字列
-	/// ### Returns
-	/// `serde_json::Value`
-	pub fn parse_json_to_value(json_text: &str) -> std::option::Option<serde_json::Value> {
-		let result = serde_json::from_str::<serde_json::Value>(json_text);
-		if result.is_err() {
-			return None;
-		}
-		return Some(result.ok().unwrap());
-	}
+/// ファイル、あるいはディレクトリーの名前部分を返します。
+///
+/// ### Arguments
+/// * `path` ファイルのパス
+///
+/// ### Returns
+/// ファイル名
+pub fn get_file_name(path: &str) -> String {
+	let file = std::path::Path::new(path);
+	return file.file_name().unwrap().to_str().unwrap().to_string();
+}
 
-	/// ファイル、あるいはディレクトリーの名前部分を返します。
-	///
-	/// ### Arguments
-	/// * `path` ファイルのパス
-	/// ### Returns
-	/// ファイル名
-	pub fn get_file_name(path: &str) -> String {
-		let file = std::path::Path::new(path);
-		return file.file_name().unwrap().to_str().unwrap().to_string();
-	}
+fn select(left: &str, right: &str) -> String {
+	return if left != "" { left.to_string() } else { right.to_string() };
 }
 
 ///
-/// コンフィギュレーション関連操作を提供します。
+/// コンフィギュレーション
 ///
-mod configuration {
+pub struct Configuration {
+	/// Slack BOT の access_token
+	pub access_token: String,
+}
 
-	use super::util;
-
+impl Configuration {
+	/// コンフィギュレーションオブジェクトを初期化します。
 	///
-	/// コンフィギュレーション
-	///
-	pub struct Configuration {
-		/// Slack BOT の access_token
-		pub access_token: String,
-		/// 投稿チャネル
-		pub channel: String,
+	/// ### Returns
+	/// `Configuration` の新しいインスタンスを返します。
+	pub fn new() -> std::result::Result<Configuration, Box<dyn std::error::Error>> {
+		let conf = Configuration { access_token: String::new() };
+		return Ok(conf);
 	}
 
-	impl Configuration {
-		/// コンフィギュレーションオブジェクトを初期化します。
-		///
-		/// ### Returns
-		/// `Configuration` の新しいインスタンスを返します。
-		pub fn new() -> std::result::Result<Configuration, Box<dyn std::error::Error>> {
-			let conf = Configuration {
-				access_token: String::new(),
-				channel: "".to_string(),
-			};
-			return Ok(conf);
-		}
+	/// コンフィギュレーションを行います。
+	pub fn configure(&mut self) -> std::result::Result<(), Box<dyn std::error::Error>> {
+		// テキストファイル全体を読み込み
+		let content = read_text_file(".settings.json")?;
 
-		/// コンフィギュレーションを行います。
-		pub fn configure(&mut self) -> std::result::Result<(), Box<dyn std::error::Error>> {
-			// テキストファイル全体を読み込み
-			let content = util::read_text_file(".settings.json")?;
-
-			// JSON をパース
-			let settings = util::parse_json_to_value(&content);
-			if settings.is_none() {
-				return Ok(());
-			}
-			let settings = settings.unwrap();
-
-			// Slack BOT Access Token
-			let access_token = settings["access_token"].as_str().unwrap();
-			self.access_token = access_token.to_string();
-
-			// Slack チャネル
-			let channel = settings["channel"].as_str().unwrap();
-			self.channel = channel.to_string();
-
+		// JSON をパース
+		let settings = parse_json_to_value(&content);
+		if settings.is_none() {
 			return Ok(());
 		}
+		let settings = settings.unwrap();
+
+		// Slack BOT Access Token
+		let access_token = settings["access_token"].as_str().unwrap();
+		self.access_token = access_token.to_string();
+
+		return Ok(());
 	}
 }
 
-//
-// アプリケーション本体に関連した操作を提供します。
-//
-mod application {
+///
+/// アプリケーション
+///
+pub struct SlackClient {
+	conf: std::option::Option<Configuration>,
+}
 
-	use super::configuration;
-	use super::util;
-
+impl SlackClient {
+	/// アプリケーションのインスタンスを返します。
 	///
-	/// アプリケーション
-	///
-	pub struct Application {
-		conf: std::option::Option<configuration::Configuration>,
+	/// ### Returns
+	/// `SlackClient` の新しいインスタンス
+	pub fn new() -> std::result::Result<SlackClient, Box<dyn std::error::Error>> {
+		let app = SlackClient { conf: None };
+		return Ok(app);
 	}
 
-	impl Application {
-		/// アプリケーションのインスタンスを返します。
-		///
-		/// ### Returns
-		/// `Application` の新しいインスタンス
-		pub fn new() -> std::result::Result<Application, Box<dyn std::error::Error>> {
-			let app = Application { conf: None };
-			return Ok(app);
-		}
-
-		/// コンフィギュレーション
-		///
-		/// ### Returns
-		/// 内部で保持している `configuration::Configuration` への参照
-		pub fn configure(&mut self) -> std::result::Result<Box<&configuration::Configuration>, Box<dyn std::error::Error>> {
-			if self.conf.is_some() {
-				let conf = self.conf.as_ref().unwrap();
-				return Ok(Box::new(conf));
-			}
-			let mut conf = configuration::Configuration::new()?;
-			conf.configure()?;
-			self.conf = Some(conf);
+	/// コンフィギュレーション
+	///
+	/// ### Returns
+	/// 内部で保持している `Configuration` への参照
+	pub fn configure(&mut self) -> std::result::Result<Box<&Configuration>, Box<dyn std::error::Error>> {
+		if self.conf.is_some() {
 			let conf = self.conf.as_ref().unwrap();
 			return Ok(Box::new(conf));
 		}
+		let mut conf = Configuration::new()?;
+		conf.configure()?;
+		self.conf = Some(conf);
+		let conf = self.conf.as_ref().unwrap();
+		return Ok(Box::new(conf));
+	}
 
-		/// コメントを付けてファイルを投稿します。
-		///
-		/// ### Arguments
-		/// * `text` コメント
-		/// * `path` ファイルへのパス
-		pub fn upload_file(&mut self, text: &str, path: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
-			// コンフィギュレーション
-			let conf = self.configure()?;
+	/// テキストメッセージを投稿します。
+	///
+	/// ### Arguments
+	/// * `channel` チャネル
+	/// * `text` コメント
+	pub fn post_text(&mut self, channel: &str, text: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
+		// コンフィギュレーション
+		let conf = self.configure()?;
 
-			// multipart/form-data を作成
-			let form = reqwest::multipart::Form::new()
-			.text("initial_comment", text.to_string())
-			.text("channels", conf.channel.to_string())
-			.text("title", util::get_file_name(path))
-			.file("file", path)?;
-			
-			// リクエスト送信
-			let access_token_header = format!("Bearer {}", conf.access_token);
-			let client = reqwest::Client::new();
-			let url = "https://slack.com/api/files.upload";
-			let mut response = client
-				.post(url)
-				.header("Content-Type", "multipart/form-data")
-				.header("Authorization", access_token_header)
-				.multipart(form)
-				.send()?;
+		// multipart/form-data を作成
+		let form = reqwest::multipart::Form::new().text("text", text.to_string()).text("channel", channel.to_string());
 
-			// 応答
-			let content = response.text()?;
-			println!("{}", content);
+		// リクエスト送信
+		let access_token_header = format!("Bearer {}", conf.access_token);
+		let client = reqwest::Client::new();
+		let url = "https://slack.com/api/chat.postMessage";
+		let mut response = client
+			.post(url)
+			.header("Content-Type", "multipart/form-data")
+			.header("Authorization", access_token_header)
+			.multipart(form)
+			.send()?;
 
-			// JSON を分解してフィールドを読み取る場合
-			if true {
-				let value = serde_json::from_str::<serde_json::Value>(content.as_str())?;
-				println!("[TRACE] {}", value);
-				println!("[TRACE] {}", value["error"].as_str().unwrap_or_default());
-				println!("[TRACE] {:?}", value["ok"]);
-				println!("[TRACE] {:?}", value["response_metadata"]);
-			}
+		// 応答
+		let content = response.text()?;
+		println!("{}", content);
 
-			return Ok(());
+		// JSON を分解してフィールドを読み取る場合
+		if true {
+			let value = serde_json::from_str::<serde_json::Value>(content.as_str())?;
+			println!("[TRACE] {}", value);
+			println!("[TRACE] {}", value["error"].as_str().unwrap_or_default());
+			println!("[TRACE] {:?}", value["ok"]);
+			println!("[TRACE] {:?}", value["response_metadata"]);
 		}
+
+		return Ok(());
+	}
+
+	/// コメントを付けてファイルを投稿します。
+	///
+	/// ### Arguments
+	/// * `channel` チャネル
+	/// * `text` コメント
+	/// * `path` ファイルへのパス
+	/// * `file_name` ファイルの表示名(省略時はファイル名が採用されます)
+	pub fn upload_file(&mut self, channel: &str, text: &str, path: &str, file_name: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
+		// コンフィギュレーション
+		let conf = self.configure()?;
+
+		let file_name = select(file_name, &get_file_name(path));
+
+		// multipart/form-data を作成
+		let form = reqwest::multipart::Form::new()
+			.text("initial_comment", text.to_string())
+			.text("channels", channel.to_string())
+			.text("title", file_name)
+			.file("file", path)?;
+
+		// リクエスト送信
+		let access_token_header = format!("Bearer {}", conf.access_token);
+		let client = reqwest::Client::new();
+		let url = "https://slack.com/api/files.upload";
+		let mut response = client
+			.post(url)
+			.header("Content-Type", "multipart/form-data")
+			.header("Authorization", access_token_header)
+			.multipart(form)
+			.send()?;
+
+		// 応答
+		let content = response.text()?;
+		println!("{}", content);
+
+		// JSON を分解してフィールドを読み取る場合
+		if true {
+			let value = serde_json::from_str::<serde_json::Value>(content.as_str())?;
+			println!("[TRACE] {}", value);
+			println!("[TRACE] {}", value["error"].as_str().unwrap_or_default());
+			println!("[TRACE] {:?}", value["ok"]);
+			println!("[TRACE] {:?}", value["response_metadata"]);
+		}
+
+		return Ok(());
 	}
 }
 
@@ -193,10 +210,16 @@ mod application {
 ///
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 	// アプリケーションオブジェクトを初期化します。
-	let mut app = application::Application::new()?;
+	let mut slack = SlackClient::new()?;
+
+	// テキストメッセージを投稿します。
+	slack.post_text(
+		"notifications",
+		"テキストメッセージ\r\n`warning`\r\n:four_leaf_clover::four_leaf_clover::four_leaf_clover::four_leaf_clover::four_leaf_clover:",
+	)?;
 
 	// コメントを付けてファイルを投稿します。
-	app.upload_file("さあうけとるがよい", "0.jpg")?;
+	slack.upload_file("notifications", "さあうけとるがよい", "0.jpg", "")?;
 
 	return Ok(());
 }
