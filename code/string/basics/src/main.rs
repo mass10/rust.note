bc1bfa1c4b9f100052f19795c40f8ba3f399631e
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-04-28 23:01:11 +0900

    wip

---
+++ code/string/basics/src/main.rs
@@ -0,0 +1,21 @@
+
+
+fn main() {
+    // [AB]
+    println!("[{}]", &"ABCDEFG"[0..2]);
+
+    // [ABCDE]
+    println!("[{}]", &"ABCDEFG"[0..5]);
+
+    // [BC]
+    println!("[{}]", &"ABCDEFG"[1..3]);
+
+    // [C]
+    println!("[{}]", &"ABCDEFG"[2..3]);
+
+    // [EFG]
+    println!("[{}]", &"ABCDEFG"[4..]);
+
+    // [A]
+    println!("[{}]", &"ABCDEFG"[..1]);
+}
