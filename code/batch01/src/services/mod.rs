2c3bed8aeda9a433b3b3db8578f2cae152dc54bb
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-05-15 18:34:12 +0900

    wip

--- code/batch01/src/services/mod.rs
+++ code/batch01/src/services/mod.rs
@@ -1,4 +1,12 @@
+//!
+//! 各種ビジネスロジックを提供するサービス層です。
+//!
+
+use crate::db;
+
+///
 /// ユーザー管理
+///
 pub struct UserManager;
 
 impl UserManager {
@@ -9,8 +17,10 @@ impl UserManager {
 
 	/// ユーザーの登録
 	pub fn register_new_user(&self, name: &str, email: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
-		let connection = super::database_connection::DatabaseConnection::new();
+		// ユーザーを登録します。
+		let connection = db::DatabaseConnection::new();
 		connection.insert(name, email);
+
 		return Ok(());
 	}
 }
