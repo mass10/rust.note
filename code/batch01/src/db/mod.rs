5ac27071b19cc90c36f45e6e3462a2e5f01c5a5e
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-05-15 18:34:03 +0900

    wip

--- code/batch01/src/db/mod.rs
+++ code/batch01/src/db/mod.rs
@@ -1,14 +1,30 @@
+//!
+//! データベース関連の操作を提供します。
+//!
+
+///
 /// データベース接続
+///
 pub struct DatabaseConnection;
 
 impl DatabaseConnection {
-	/// インスタンスを返します。
+	/// 新しいインスタンスを返します。
+	///
+	/// # Returns
+	/// `DatabaseConnection` の新しいインスタンス
 	pub fn new() -> DatabaseConnection {
 		let instance = DatabaseConnection {};
 		return instance;
 	}
 
 	/// ユーザーの登録
+	///
+	/// # Arguments
+	/// `name` ユーザーの名前
+	/// `email` ユーザーのメールアドレス
+	///
+	/// # Returns
+	/// 成否
 	pub fn insert(&self, name: &str, email: &str) -> bool {
 		println!("[TRACE] REGISTER... name: {}, email: {}", name, email);
 		return true;
