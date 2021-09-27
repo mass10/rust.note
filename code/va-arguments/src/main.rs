9a4500b21572bc33a54f8cebee589658ff22c958
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-09-27 23:15:58 +0900

    Update main.rs

--- code/va-arguments/src/main.rs
+++ code/va-arguments/src/main.rs
@@ -1,15 +1,46 @@
+//!
+//! ロギング関数の実装を試みる
+//!
+
+/// 現在のタイムスタンプを返します。
+///
+/// ### Returns
+/// タイムスタンプ
+fn get_current_timestamp() -> String {
+	let timestamp = chrono::Local::now().format("%Y-%m-%d %H:%M:%S%.3f");
+	return format!("{}", timestamp);
+}
+
 /// 愚直引数バージョン
 ///
 /// # Arguments
 /// * `args` - 引数の配列を指定します。
-fn info(args: &[&dyn std::fmt::Display]) {
+fn info1(args: &[&dyn std::fmt::Display]) {
+	print!("{} [INFO1] ", get_current_timestamp());
 	for arg in args {
 		print!("{}", arg);
 	}
 	println!();
 }
 
+#[macro_export]
+macro_rules! info2 {
+    ($($arg:tt)*) => ({
+        print!("{} [INFO2] ", get_current_timestamp());
+        print!($($arg)*);
+    })
+}
+
+// こういうことがやりたい
+// fn info2(args: ...&dyn std::fmt::Display) {
+// 	for arg in args {
+// 		print!("{}", arg);
+// 	}
+// 	println!();
+// }
+
 fn main() {
-	info(&[&"### start ###", &"aaa", &900, &1.23]);
-	info(&[&"### start ###".to_string()]);
+	info1(&[&"### start ###", &"aaa", &900, &1.23]);
+	info1(&[&"### start ###".to_string()]);
+	info2!("{} {}", 1, "abc");
 }
