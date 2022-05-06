cec6214003c122b966483abdeb9a6768c1c27836
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-05-06 18:36:25 +0900

    Update mod.rs

--- code/multithreading/example0/src/thread/mod.rs
+++ code/multithreading/example0/src/thread/mod.rs
@@ -18,6 +18,18 @@ impl Thread {
 		return Thread { _x: "".to_string() };
 	}
 
+	/// スレッド内の何かの処理
+	fn try_respond(tx: &std::sync::mpsc::Sender<String>) -> Result<bool, Box<dyn std::error::Error>> {
+		// タイムスタンプ
+		let timestamp = util::get_current_timestamp();
+		// ある条件が成立するとスレッドは終了します。
+		let exit_condition = if timestamp.ends_with("0") { true } else { false };
+		// スレッドからのメッセージをメインスレッドへ送信します。
+		let thread_message = format!("{} スレッドからのメッセージ{}", timestamp, if exit_condition { "(終了)" } else { "" });
+		tx.send(thread_message)?;
+		return Ok(exit_condition);
+	}
+
 	/// スレッドを起動します。
 	pub fn start(
 		&self,
@@ -27,19 +39,22 @@ impl Thread {
 		let handle = std::thread::spawn(move || {
 			debug!("$$$ begin thread $$$");
 
+			// 成功カウンター(スレッドの終了条件)
+			let mut success_count = 0;
+
 			loop {
 				// TODO Arc でくるんだオブジェクトを介して安全に情報の伝達ができるのか
 				// map.lock();
 				// map.as_ref().set
 
-				// タイムスタンプ
-				let timestamp = util::get_current_timestamp();
+				// ================ 終了条件 ================
 				// ある条件が成立するとスレッドは終了します。
-				let exit_condition = if timestamp.ends_with("0") { true } else { false };
+				if 10 <= success_count {
+					break;
+				}
 
-				// スレッドからのメッセージをメインスレッドへ送信します。
-				let thread_message = format!("{} スレッドからのメッセージ{}", timestamp, if exit_condition { "(終了)" } else { "" });
-				let result = tx.send(thread_message);
+				// ================ 一定期間で実施する何らかの処理 ================
+				let result = Self::try_respond(&tx);
 				if result.is_err() {
 					let error = result.err().unwrap();
 					error!("Unknown error. reason: [{}]", error);
@@ -47,11 +62,13 @@ impl Thread {
 					break;
 				}
 
-				// ある条件が成立するとスレッドは終了します。
-				if exit_condition {
-					break;
+				// 結果の確認
+				let status = result.unwrap();
+				if status {
+					success_count += 1;
 				}
 
+				// 少し待機
 				std::thread::sleep(std::time::Duration::from_millis(234));
 			}
 
