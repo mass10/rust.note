0e00e1b191cab9a884e795338492cefe3225b025
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-05-06 17:41:52 +0900

    Update mod.rs

--- code/multithreading/example0/src/util/mod.rs
+++ code/multithreading/example0/src/util/mod.rs
@@ -1,4 +1,7 @@
 /// タイムスタンプを生成します。
+///
+/// # Returns
+/// タイムスタンプ
 pub fn get_current_timestamp() -> String {
 	let date = chrono::Local::now();
 	return format!("{}", date.format("%Y-%m-%d %H:%M:%S%.3f"));
