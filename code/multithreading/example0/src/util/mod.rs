05bc25f94c724ba4da544d81a22e2244db817117
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-05-06 12:10:25 +0900

    Update mod.rs

--- code/multithreading/example0/src/util/mod.rs
+++ code/multithreading/example0/src/util/mod.rs
@@ -1,5 +1,34 @@
 /// タイムスタンプを生成します。
-pub fn get_timestamp() -> String {
+pub fn get_current_timestamp() -> String {
 	let date = chrono::Local::now();
 	return format!("{}", date.format("%Y-%m-%d %H:%M:%S%.3f"));
+}
+
+/// 簡易ロギング(debug)
+#[macro_export]
+macro_rules! debug {
+	($($arg:tt)*) => ({
+		let line = format!($($arg)*);
+		println!("{} [DEBUG] {} ({:?}) {}", util::get_current_timestamp(), std::process::id(), std::thread::current().id(), line);
+	})
+}
+
+/// 簡易ロギング(info)
+#[macro_export]
+macro_rules! info {
+	($($arg:tt)*) => ({
+		let line = format!($($arg)*);
+		println!("{} [INFO] {} ({}) {}", util::get_current_timestamp(),
+			std::process::id(), std::thread::current().id(), line);
+	})
+}
+
+/// 簡易ロギング(error)
+#[macro_export]
+macro_rules! error {
+	($($arg:tt)*) => ({
+		let line = format!($($arg)*);
+		println!("{} [ERROR] {} ({:?}) {}",
+			util::get_current_timestamp(), std::process::id(), std::thread::current().id(), line);
+	})
 }
