763fc3fc99870b2ce9e5fc9e4da660c569fc6f4c
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-06-19 12:18:06 +0900

    Update main.rs

--- code/pwd/src/main.rs
+++ code/pwd/src/main.rs
@@ -1,12 +1,6 @@
 /// エントリーポイント
 fn main() {
 	let result = std::env::current_dir();
-	if result.is_err() {
-		println!("{}", result.unwrap_err());
-		return;
-	}
-
 	let current_directory = result.unwrap();
-
 	print!("{}", current_directory.as_path().to_str().unwrap());
 }
