9e4a62997ad4d9643511af913f4a64fd0caacab0
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-06-25 10:57:11 +0900

    -

--- code/coloring-console-characters/src/main.rs
+++ code/coloring-console-characters/src/main.rs
@@ -1,38 +1,38 @@
 mod decorator {
 
-    pub fn make_black(s: &str) -> String {
+    pub fn black(s: &str) -> String {
         return format!("\x1b[30m{}\x1b[0m", s);
     }
 
-    pub fn make_red(s: &str) -> String {
+    pub fn red(s: &str) -> String {
         return format!("\x1b[31m{}\x1b[0m", s);
     }
 
-    pub fn make_green(s: &str) -> String {
+    pub fn green(s: &str) -> String {
         return format!("\x1b[32m{}\x1b[0m", s);
     }
 
-    pub fn make_yellow(s: &str) -> String {
+    pub fn yellow(s: &str) -> String {
         return format!("\x1b[33m{}\x1b[0m", s);
     }
 
-    pub fn make_blue(s: &str) -> String {
+    pub fn blue(s: &str) -> String {
         return format!("\x1b[34m{}\x1b[0m", s);
     }
 
-    pub fn make_magenta(s: &str) -> String {
+    pub fn magenta(s: &str) -> String {
         return format!("\x1b[35m{}\x1b[0m", s);
     }
 
-    pub fn make_cyan(s: &str) -> String {
+    pub fn cyan(s: &str) -> String {
         return format!("\x1b[36m{}\x1b[0m", s);
     }
 
-    pub fn make_white(s: &str) -> String {
+    pub fn white(s: &str) -> String {
         return format!("\x1b[37m{}\x1b[0m", s);
     }
 
-    pub fn make_bold(s: &str) -> String {
+    pub fn bold(s: &str) -> String {
         return format!("\x1b[1m{}\x1b[0m", s);
     }
 }
@@ -40,21 +40,21 @@ mod decorator {
 
 fn main() {
 
-    println!("{}", decorator::make_black("COLORING EXAMPLE"));
-    println!("{}", decorator::make_red("COLORING EXAMPLE"));
-    println!("{}", decorator::make_green("COLORING EXAMPLE"));
-    println!("{}", decorator::make_yellow("COLORING EXAMPLE"));
-    println!("{}", decorator::make_blue("COLORING EXAMPLE"));
-    println!("{}", decorator::make_magenta("COLORING EXAMPLE"));
-    println!("{}", decorator::make_cyan("COLORING EXAMPLE"));
-    println!("{}", decorator::make_white("COLORING EXAMPLE"));
+    println!("{}", decorator::black("COLORING EXAMPLE"));
+    println!("{}", decorator::red("COLORING EXAMPLE"));
+    println!("{}", decorator::green("COLORING EXAMPLE"));
+    println!("{}", decorator::yellow("COLORING EXAMPLE"));
+    println!("{}", decorator::blue("COLORING EXAMPLE"));
+    println!("{}", decorator::magenta("COLORING EXAMPLE"));
+    println!("{}", decorator::cyan("COLORING EXAMPLE"));
+    println!("{}", decorator::white("COLORING EXAMPLE"));
 
-    println!("{}", decorator::make_bold(&decorator::make_black("COLORING EXAMPLE")));
-    println!("{}", decorator::make_bold(&decorator::make_red("COLORING EXAMPLE")));
-    println!("{}", decorator::make_bold(&decorator::make_green("COLORING EXAMPLE")));
-    println!("{}", decorator::make_bold(&decorator::make_yellow("COLORING EXAMPLE")));
-    println!("{}", decorator::make_bold(&decorator::make_blue("COLORING EXAMPLE")));
-    println!("{}", decorator::make_bold(&decorator::make_magenta("COLORING EXAMPLE")));
-    println!("{}", decorator::make_bold(&decorator::make_cyan("COLORING EXAMPLE")));
-    println!("{}", decorator::make_bold(&decorator::make_white("COLORING EXAMPLE")));
+    println!("{}", decorator::bold(&decorator::black("COLORING EXAMPLE")));
+    println!("{}", decorator::bold(&decorator::red("COLORING EXAMPLE")));
+    println!("{}", decorator::bold(&decorator::green("COLORING EXAMPLE")));
+    println!("{}", decorator::bold(&decorator::yellow("COLORING EXAMPLE")));
+    println!("{}", decorator::bold(&decorator::blue("COLORING EXAMPLE")));
+    println!("{}", decorator::bold(&decorator::magenta("COLORING EXAMPLE")));
+    println!("{}", decorator::bold(&decorator::cyan("COLORING EXAMPLE")));
+    println!("{}", decorator::bold(&decorator::white("COLORING EXAMPLE")));
 }
