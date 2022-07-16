2d89ad11f6724b589d2a548cc41f60650cc5699e
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-07-16 12:14:44 +0900

    Create core.rs

---
+++ code/filesystem/diagnose-file-types/src/core.rs
@@ -0,0 +1,74 @@
+use crate::configuration::Configuration;
+
+/// 拡張子診断クラス
+pub struct Calculator<'a> {
+	/// コンフィギュレーションへの参照
+	conf: &'a Configuration,
+	/// 拡張子と件数を管理します。
+	map: std::collections::HashMap<String, u32>,
+}
+
+impl<'a> Calculator<'a> {
+	/// `Calculator` の新しいインスタンスを返します。
+	///
+	/// # Returns
+	/// `Calculator` の新しいインスタンス
+	pub fn new(conf: &'a Configuration) -> Self {
+		return Self { conf: conf, map: std::collections::HashMap::new() };
+	}
+
+	/// 診断結果を出力します。
+	pub fn summary(&mut self) {
+		let mut total: u32 = 0;
+		for e in &self.map {
+			println!("{}\t{}", e.0, e.1);
+			total += e.1;
+		}
+		println!("TOTAL: {:?}", total);
+	}
+
+	/// ファイルパスを診断します。
+	///
+	/// # Arguments
+	/// `path` ファイルのパス
+	pub fn diagnose(&mut self, path: &std::path::Path) -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
+		let _conf = self.conf;
+
+		// ファイル名
+		let name = path.file_name().unwrap_or_default();
+		let name = name.to_str();
+		if name.is_none() {
+			println!("[WARN] Cannot retrieve file name of [{:?}].", path);
+			return Ok(());
+		}
+		let name = name.unwrap();
+
+		// 拡張子
+		let result = path.extension();
+		if result.is_none() {
+			// println!("[WARN] Cannot retrieve ext of [{:?}].", path);
+			return Ok(());
+		}
+		let extension = result.unwrap();
+		let result = extension.to_str();
+		if result.is_none() {
+			println!("[WARN] Cannot retrieve ext of [{:?}].", extension);
+			return Ok(());
+		}
+		let extension = result.unwrap();
+
+		println!("> [{}] >> [{}] + [{}]", path.as_os_str().to_str().unwrap(), name, extension);
+
+		let value = self.map.get_key_value(extension);
+		if value.is_none() {
+			// 初めての拡張子
+			self.map.insert(extension.to_string(), 1);
+		} else {
+			// 既知の拡張子
+			let current = value.unwrap().1;
+			let new_value = *current;
+			self.map.insert(extension.to_string(), new_value + 1);
+		}
+		return Ok(());
+	}
+}
