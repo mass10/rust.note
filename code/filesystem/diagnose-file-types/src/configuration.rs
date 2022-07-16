7ae860fe8f0e1dfa8d1998eb2cd7f32bb6b5feea
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-07-16 16:17:59 +0900

    Update configuration.rs

--- code/filesystem/diagnose-file-types/src/configuration.rs
+++ code/filesystem/diagnose-file-types/src/configuration.rs
@@ -42,13 +42,13 @@ fn find_settings_toml() -> std::result::Result<String, std::boxed::Box<dyn std::
 #[derive(std::fmt::Debug)]
 pub struct Configuration {
 	/// 走査するパス
-	path_to_run: String,
+	pub path_to_run: String,
 
 	/// 除外するディレクトリ名
-	exclude_dirs: std::collections::HashSet<String>,
+	pub exclude_dirs: std::collections::HashSet<String>,
 
 	/// 除外するファイル名
-	exclude_files: std::collections::HashSet<String>,
+	pub exclude_files: std::collections::HashSet<String>,
 }
 
 // TOML の内容
@@ -99,33 +99,6 @@ impl Configuration {
 		conf.configure(&path)?;
 
 		return Ok(conf);
-	}
-
-	/// 走査するパス
-	pub fn get_path_to_run(&self) -> &String {
-		return &self.path_to_run;
-	}
-
-	/// 除外フォルダーリスト
-	///
-	/// 参照を返すことによって、ループが確実に参照イテレーションになります。
-	/// (何も考えずに for は、参照イテレーションではなく、要素の反復 move のような実装のようです。
-	///
-	/// # Returns
-	/// フォルダーリスト
-	pub fn get_exclude_dirs(&self) -> &std::collections::HashSet<String> {
-		return &self.exclude_dirs;
-	}
-
-	/// 除外ファイルリスト
-	///
-	/// 参照を返すことによって、ループが確実に参照イテレーションになります。
-	/// 通常の for は、参照イテレーションではなく、要素の反復 move のような実装のようです。
-	///
-	/// # Returns
-	/// ファイルリスト
-	pub fn get_exclude_files(&self) -> &std::collections::HashSet<String> {
-		return &self.exclude_files;
 	}
 
 	/// コンフィギュレーション
