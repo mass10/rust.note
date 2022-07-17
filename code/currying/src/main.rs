bfefd91420cb5e8660f7dbf465a119ccd097a6bb
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-07-17 21:13:13 +0900

    Update main.rs

--- code/currying/src/main.rs
+++ code/currying/src/main.rs
@@ -1,12 +1,29 @@
-fn add(base: i32) -> impl Fn(i32) -> i32 {
+/// 特別なプリンターを返します。
+/// 
+/// # Arguments
+/// * `prefix` プレフィクス
+fn printer(prefix: &str) -> impl Fn (i32) + '_ {
+	return move |n: i32| {
+		println!("{}{}", prefix, n);
+	};
+}
+
+/// `base` をもとに加算する関数を返します。
+/// 
+/// # Arguments
+/// * `base` もとの数
+fn multiply(base: i32) -> impl Fn(i32) -> i32 {
 	let f = move |n| {
 		return base * n;
 	};
 	return f;
 }
 
+/// アプリケーションのエントリーポイントです。
 fn main() {
-	println!("{}", add(1)(2)); // 2
-	println!("{}", add(2)(3)); // 6
-	println!("{}", add(3)(4)); // 12
+	let printer = printer("> ");
+
+	printer(multiply(1)(2)); // > 2
+	printer(multiply(2)(3)); // > 6
+	printer(multiply(3)(4)); // > 12
 }
