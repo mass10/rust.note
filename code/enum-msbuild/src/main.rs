fbd058bac8cd770124859355e8f268d2d2ccad02
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-09-01 00:12:57 +0900

    Update main.rs

--- code/enum-msbuild/src/main.rs
+++ code/enum-msbuild/src/main.rs
@@ -1,71 +1,129 @@
 struct Configuration {}
 
 impl Configuration {
-    fn new() -> Configuration {
-        return Configuration {};
-    }
+	fn new() -> Configuration {
+		return Configuration {};
+	}
 }
+
+fn get_file_name(path: &std::path::Path) -> &str {
+	let name = path.file_name();
+	if name.is_none() {
+		return "";
+	}
+	let name = name.unwrap().to_str();
+	if name.is_none() {
+		return "";
+	}
+	return name.unwrap();
+}
+
+fn get_parent_file_name(path: &std::path::Path) -> &str {
+	let parent = path.parent();
+	if parent.is_none() {
+		return "";
+	}
+	return get_file_name(parent.unwrap());
+}
+
+/// ファイルハンドラー
+///
+/// ※型に置き換えるとコンパイルエラーになる🔥
+#[allow(unused)]
+type FileHandler = dyn FnMut(&std::path::Path) -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>>;
 
 /// ファイル走査
 ///
 /// # Arguments
 /// * `e` パス
 /// * `handler` ファイルハンドラー
-pub fn search(
-    conf: &Configuration,
-    path: &std::path::Path,
-    handler: &mut dyn FnMut(
-        &std::path::Path,
-    ) -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>>,
-) -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
-    if !path.exists() {
-        println!("[TRACE] invalid path {}", path.to_str().unwrap());
-        return Ok(());
-    }
+fn search(conf: &Configuration, path: &str, handler: &mut dyn FnMut(&str) -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>>) -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
+	let unknown = std::path::Path::new(path);
+	if !unknown.exists() {
+		// println!("[TRACE] invalid path {}", path.to_str().unwrap());
+		return Ok(());
+	}
 
-    if path.is_dir() {
-        let name = retrieve_name(path).unwrap();
+	// println!("[TRACE] FOUND [{}]", path.to_str().unwrap());
 
-        // // 名前のフィルタリング
-        // for pat in &conf.exclude_dirs {
-        //     // TODO: ちゃんとする
-        //     if &name == pat {
-        //         return Ok(());
-        //     }
-        // }
+	if unknown.is_dir() {
+		// 親ディレクトリの名前
+		let parent_directory_name = get_parent_file_name(&unknown);
+		// このディレクトリの名前
+		let directory_name = get_file_name(unknown);
 
-        let it = std::fs::read_dir(path)?;
-        for e in it {
-            let entry = e.unwrap();
-            let entry_path = entry.path();
-            search(conf, &entry_path, handler)?;
-        }
-        return Ok(());
-    } else if path.is_file() {
-        let name = retrieve_name(path).unwrap();
+		if parent_directory_name == "" {
+			// ドライブ、もしくは第一階層 (親なし)
+			if directory_name == "" {
+				// ドライブ
+				println!("[DEBUG] ドライブ [{}] -> [{}] ({})", parent_directory_name, path, directory_name);
+			} else if directory_name.contains("Program Files") {
+				println!("[DEBUG] ドライブ [{}] -> [{}] ({})", parent_directory_name, path, directory_name);
+			} else {
+				// 第一階層
+				// ここはもう掘らない
+				return Ok(());
+			}
+		} else if parent_directory_name.contains("Program Files") {
+			// Program Files* の直下
+			if directory_name.contains("MSBuild") {
+				println!("[DEBUG] その他のディレクトリ [{}] -> [{}]", parent_directory_name, path);
+			} else if directory_name.contains("Microsoft Visual Studio") {
+				println!("[DEBUG] その他のディレクトリ [{}] -> [{}]", parent_directory_name, path);
+			} else {
+				// ここはもう掘らない
+				return Ok(());
+			}
+		} else {
+			// println!("[DEBUG] ？ [{}] -> [{}]", parent_directory_name, path);
+		}
 
-        // 名前のフィルタリング
-        // for pat in &conf.exclude_files {
-        //     // TODO: ちゃんとする
-        //     if name.contains(pat) {
-        //         return Ok(());
-        //     }
-        // }
+		let it = std::fs::read_dir(path)?;
+		for e in it {
+			let entry = e.unwrap();
+			let entry_path = entry.path();
+			let entry_path = entry_path.to_str().unwrap();
+			let _result = search(conf, &entry_path, handler);
+		}
+		return Ok(());
+	} else if unknown.is_file() {
+		// let name = path.file_name().unwrap();
+		// let name = retrieve_name(path).unwrap();
 
-        return handler(path);
-    } else {
-        println!("[WARN] 不明なファイルシステム {:?}", path);
-    }
-    return Ok(());
+		return handler(path);
+	} else {
+		println!("[WARN] 不明なファイルシステム {:?}", path);
+	}
+	return Ok(());
 }
 
 fn main() {
-    let conf = Configuration::new();
+	let conf = Configuration::new();
 
-    let result = search(&conf, "C:\\");
-    if result.is_err() {
-        let err = result.err().unwrap();
-        println!("ERROR: {}", err);
-        return;
-    }
+	let path = "C:\\";
+
+	// ハンドラー
+	let mut handler = |arg: &str| -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
+		let unknown = std::path::Path::new(arg);
+		if !unknown.is_file() {
+			return Ok(());
+		}
+		let file_name = get_file_name(unknown);
+		// let path_as_str = arg;
+		if !file_name.ends_with(".exe") {
+			return Ok(());
+		}
+		if !file_name.contains("MSBuild") {
+			return Ok(());
+		}
+		println!("[DEBUG] ファイルを検出 [{}]", arg);
+		return Ok(());
+	};
+
+	let result = search(&conf, path, &mut handler);
+	if result.is_err() {
+		let err = result.err().unwrap();
+		println!("ERROR: {}", err);
+		return;
+	}
 }
