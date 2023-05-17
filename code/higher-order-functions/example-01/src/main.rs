f7d89fd20ea910bbd5b6eb69f2e7272293c580b9
Irisawa Masaru <irisawa.masaru@gmail.com>
2023-05-17 23:42:46 +0900

    Update main.rs

--- code/higher-order-functions/example-01/src/main.rs
+++ code/higher-order-functions/example-01/src/main.rs
@@ -1,35 +1,91 @@
-fn function01(message: String) -> std::result::Result<(), String> {
+/// アプリケーション固有のエラー
+#[derive(Debug, Clone)]
+pub struct ApplicationError {
+	/// メッセージ
+	pub message: String,
+}
+
+impl ApplicationError {
+	/// エラーを生成する
+	#[allow(unused)]
+	pub fn new(message: &str) -> ApplicationError {
+		return ApplicationError { message: message.to_string() };
+	}
+}
+
+impl std::fmt::Display for ApplicationError {
+	/// [std::fmt::Display] としての振る舞いを実装しています。
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+		write!(f, "{}", self.message)
+	}
+}
+
+impl std::error::Error for ApplicationError {
+	/// [std::error::Error] としての振る舞いを実装しています。
+	fn description(&self) -> &str {
+		&self.message
+	}
+}
+
+
+type AbstractTask = Box<dyn Fn(&str) -> std::result::Result<(), Box<dyn std::error::Error>>>;
+
+/// とある操作01
+fn function01(message: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
 	println!("[TRACE] <function01()> {}", message);
 	return Ok(());
 }
 
-fn function02(message: String) -> std::result::Result<(), String> {
+/// とある操作02
+fn function02(message: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
 	println!("[TRACE] <function02()> {}", message);
 	return Ok(());
 }
 
-fn nop(_: String) -> std::result::Result<(), String> {
-	return Err(String::from("NOP"));
+/// とある操作(nop)
+fn nop(_: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
+	// let resson = ApplicationError::new("NOP");
+	// return Err(Box::new(resson));
+	return Ok(());
 }
 
-// ファンクションを返す関数
-fn get_function(function_id: String) -> std::boxed::Box<Fn(String) -> std::result::Result<(), String>> {
-	match function_id.as_str() {
-		"01" => std::boxed::Box::new(function01),
-		"02" => std::boxed::Box::new(function02),
-		_ => std::boxed::Box::new(nop),
-	}
+/// 要求された識別子に該当するファンクションを返す関数
+fn get_function(function_id: &str) -> Result<AbstractTask, Box<dyn std::error::Error>> {
+	let operation = match function_id {
+		"01" => function01,
+		"02" => function02,
+		_ => nop,
+	};
+	return Ok(Box::new(operation));
 }
 
+/// エントリーポイント
 fn main() {
+	// コマンドライン引数
 	let args: Vec<String> = std::env::args().collect();
 	if args.len() < 2 {
 		return;
 	}
+
+	// 第一引数がリクエストです。
 	let function_id = &args[1];
-	// find operation
-	let unknown = get_function(function_id.to_string());
-	// invoke
-	let result = unknown(String::from("こにちは"));
-	println!("[TRACE] <run()> {:?}", result);
+
+	// 要求された機能に該当するファンクションを取得します。
+	let result = get_function(&function_id);
+	if result.is_err() {
+		let error = result.err().unwrap();
+		println!("[ERROR] <main()> {}", error);
+		return;
+	}
+
+	// 要求された処理を実行します。
+	let task = result.unwrap();
+	let result = task("こにちは");
+	if result.is_err() {
+		let error = result.err().unwrap();
+		println!("[ERROR] <main()> {}", error);
+		return;
+	}
+
+	println!("[TRACE] Ok.");
 }
