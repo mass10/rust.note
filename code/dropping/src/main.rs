4371cd868fa96c95b79954e600acaa8bb9d0de43
Irisawa Masaru <irisawa.masaru@gmail.com>
2023-06-10 19:57:45 +0900

    wip

--- code/dropping/src/main.rs
+++ code/dropping/src/main.rs
@@ -22,7 +22,7 @@ impl Drop for Lock {
 }
 
 struct MyLifetime {
-	pub name: String
+	pub name: String,
 }
 
 impl Drop for MyLifetime {
@@ -34,34 +34,49 @@ impl Drop for MyLifetime {
 
 /// アプリケーションの生存期間を示す構造体
 struct ApplicationLifetime {
-	pub exit_code: i32
+	pub exit_code: i32,
 }
 
 /// アプリケーション解放の実装
 impl Drop for ApplicationLifetime {
 	fn drop(&mut self) {
 		if self.exit_code != 0 {
-			println!("[ApplicationLifetime] QUIT IMMEDIATELY. (with exit_code: {})", self.exit_code);
+			println!(
+				"[ApplicationLifetime] QUIT IMMEDIATELY. (with exit_code: {})",
+				self.exit_code
+			);
 			// 残っているすべての構造体に対して Drop が呼び出されません。
 			std::process::exit(self.exit_code);
 		}
-		println!("[ApplicationLifetime] QUIT NATURALLY. (with exit_code: {})", self.exit_code);
+		println!(
+			"[ApplicationLifetime] QUIT NATURALLY. (with exit_code: {})",
+			self.exit_code
+		);
 	}
 }
 
 /// エントリーポイント
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let _longest_lifetime = MyLifetime { name: "longest".to_string() };
+	let _longest_lifetime = MyLifetime {
+		name: "longest".to_string(),
+	};
 	let mut _application_lifetime = ApplicationLifetime { exit_code: 0 };
-	let _medium_lifetime = MyLifetime { name: "medium".to_string() };
-	let _shortest_lifetime = MyLifetime { name: "short".to_string() };
+	let _medium_lifetime = MyLifetime {
+		name: "medium".to_string(),
+	};
+	let _shortest_lifetime = MyLifetime {
+		name: "short".to_string(),
+	};
 
 	println!("### START ###");
 
 	let _dummy = Lock::new();
 
 	// 現在のミリ秒
-	let current_millisec = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH)?.as_millis() % 1000;
+	let current_millisec = std::time::SystemTime::now()
+		.duration_since(std::time::UNIX_EPOCH)?
+		.as_millis()
+		% 1000;
 
 	// ランダムで終了コードを設定
 	if 500 <= current_millisec {
