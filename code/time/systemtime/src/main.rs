4e0ecf836fa2343e68884433cc81d9d0d874edd8
Masaru Irisawa 入澤 賢 <irisawa.masaru@gmail.com>
2024-09-07 11:59:08 +0900

    Update main.rs

--- code/time/systemtime/src/main.rs
+++ code/time/systemtime/src/main.rs
@@ -1,30 +1,56 @@
+//!
+//! 様々なシステム日時を取得するサンプル
+//!
+
 extern crate chrono;
 
-fn timestamp0() -> String {
+fn get_current_timestamp0() -> String {
 	let date = chrono::Local::now();
 	return format!("{}", date.format("%Y-%m-%d %H:%M:%S%.3f"));
 }
 
-fn timestamp1() -> String {
+fn get_current_timestamp1() -> String {
 	let date = chrono::Local::now();
 	return format!("{}", date.format("%Y%m%d-%H%M%S"));
 }
 
-fn timestamp_with_timezone() -> String {
+/// 現在のタイムスタンプを取得する(書式付き)
+fn get_current_timestamp_with_timezone() -> String {
 	let date = chrono::Local::now();
 	return format!("{}", date.format("%+"));
 }
 
+// fn generate_timestamnp_in(hours: i64) -> chrono::DateTime<chrono::Utc> {
+fn generate_timestamnp_in(hours: i64) -> String {
+    let now = chrono::Utc::now();
+    let local_time = now + chrono::Duration::hours(hours);
+	let text = format!("{} {:02?}", local_time.format("%Y-%m-%d %H:%M:%S%.3f"), hours);
+    return text;
+}
+
+/// 任意のソーンで現在のタイムスタンプを取得します。
+fn get_current_timestamp_with_timezone2() -> String {
+	let date = chrono::Local::now();
+	return format!("{}", date.format("%Y-%m-%d %H:%M:%S%.3f %Z"));
+}
+
+/// エントリーポイント
 fn main() {
 	println!("# std::time::SystemTime");
 	println!("{:?}", std::time::SystemTime::now());
 	println!();
 
 	println!("# timestamp with timezone (chrono::Local)");
-	println!("{}", timestamp_with_timezone());
+	println!("{}", get_current_timestamp_with_timezone());
+	println!("{}", get_current_timestamp_with_timezone2());
+	println!();
+
+	for i in -12..12 {
+		println!("{}", generate_timestamnp_in(i));
+	}
 	println!();
 
 	println!("# local timestamp");
-	println!("{}", timestamp0());
-	println!("{}", timestamp1());
+	println!("{}", get_current_timestamp0());
+	println!("{}", get_current_timestamp1());
 }
