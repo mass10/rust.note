ccf10097d24528cab5207b9f0058e5ac6f7cf816
Masaru Irisawa 入澤 賢 <irisawa.masaru@gmail.com>
2024-12-02 12:52:29 +0900

    Update main.rs

--- code/network/slack-api/src/main.rs
+++ code/network/slack-api/src/main.rs
@@ -9,15 +9,31 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
 	// テキストメッセージを投稿します。
 	slack.post_text(
+		// channel name or channel ID
 		"notifications",
+		// 書式付きテキストメッセージ
 		"テキストメッセージ\r\n`warning`\r\n:four_leaf_clover::four_leaf_clover::four_leaf_clover::four_leaf_clover::four_leaf_clover:",
 	)?;
 
 	// DM を送信します。
-	slack.post_text("member ID or e-mail", "ダイレクトメッセージです。")?;
+	slack.post_text(
+		// member ID or e-mail
+		"member ID or e-mail",
+		// テキストメッセージ
+		"ダイレクトメッセージです。",
+	)?;
 
 	// コメントを付けてファイルを投稿します。
-	slack.post_file("notifications", "さあうけとるがよい", "0.jpg", "")?;
+	slack.post_file(
+		// channel name or channel ID
+		"notifications",
+		// テキストメッセージ
+		"さあうけとるがよい",
+		// ファイルパス
+		"0.jpg",
+		// ファイル名
+		"",
+	)?;
 
 	return Ok(());
 }
